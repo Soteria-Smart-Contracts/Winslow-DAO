3e1545daae65a86079361b348d2411af91cfe25b
Darius Tanz <scratchticketseb@gmail.com>
2022-10-04 15:26:35 +0000

    Oct 4, 2022, 11:26 AM

--- Eros Standard/ErosTemplateV1.sol
+++ Eros Standard/ErosTemplateV1.sol
@@ -29,6 +29,6 @@ interface EROSDAO{
     function CheckErosApproval(address) public view returns(bool);
 }
 
-interface EROSDAO{
+interface ExContract{
     function CheckErosApproval(address) public view returns(bool);
 }
