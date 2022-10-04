c0f55fde0d827f5657e9c6e9c2777c5dca2fbc33
Darius Tanz <scratchticketseb@gmail.com>
2022-10-04 15:30:25 +0000

    Oct 4, 2022, 11:30 AM

--- Eros Standard/ErosTemplateV1.sol
+++ Eros Standard/ErosTemplateV1.sol
@@ -8,7 +8,7 @@ contract ErosProposal{
     bool public ErosRepeatable = false; // Defines if the execute fuction is designed to be called multiple times in the future
 
     //Additional variables can be added here
-    
+    address public ExternalContract = 
     //Additional variables can be added here
 
     constructor(bool Repeatable){
