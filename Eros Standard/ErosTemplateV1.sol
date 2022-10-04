16d87008dbeb081e93736e334773bac39ee16d2d
Darius Tanz <scratchticketseb@gmail.com>
2022-10-04 15:17:47 +0000

    Oct 4, 2022, 11:17 AM

--- Eros Standard/ErosTemplateV1.sol
+++ Eros Standard/ErosTemplateV1.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.17;
 contract ErosProposal{
     address public DAO = 0x0000000000000000000000000000000000000000;
     bool public ErosRepeatable;
+
     //Additional variables can be added here
 
     constructor{
