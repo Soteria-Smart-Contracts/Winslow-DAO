7dc63453be56452deb2e60b831d5372c9c263887
Darius Tanz <scratchticketseb@gmail.com>
2022-10-04 15:25:50 +0000

    Oct 4, 2022, 11:25 AM

--- Eros Standard/ErosTemplateV1.sol
+++ Eros Standard/ErosTemplateV1.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.17;
 
 contract ErosProposal{
     address public DAO = 0x0000000000000000000000000000000000000000;
-    bool public Executed; //
+    bool public Executed;
     bool public ErosRepeatable; // Defines if the execute fuction is designed to be called multiple times in the future
 
     //Additional variables can be added here
