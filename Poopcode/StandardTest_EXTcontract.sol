2cadc94b2728fd09a08b8ce7976ba8b594392285
Darius Tanz <scratchticketseb@gmail.com>
2022-10-04 17:13:49 +0000

    Oct 4, 2022, 1:13 PM

--- Poopcode/StandardTest_EXTcontract.sol
+++ Poopcode/StandardTest_EXTcontract.sol
@@ -5,6 +5,11 @@ contract ImExternal{
     string public Information;
     uint public Integer;
 
+    modifier OnlyDAO{ //This same modifier must be used on external contracts called by this contract
+        require(msg.sender == DAO  || EROSDAO(DAO).CheckErosApproval(address(this)), "The caller is either not the DAO or not approved by the DAO");
+        _;
+    }
+
     function UpdateString() public 
 
 
