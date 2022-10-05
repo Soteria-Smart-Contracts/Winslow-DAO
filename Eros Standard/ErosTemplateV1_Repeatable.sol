d353118b58f527cb35c06fbce825eedd2dbb00b2
Darius Tanz <scratchticketseb@gmail.com>
2022-10-05 00:28:50 +0000

    Oct 4, 2022, 8:28 PM

--- Eros Standard/ErosTemplateV1_Repeatable.sol
+++ Eros Standard/ErosTemplateV1_Repeatable.sol
@@ -20,7 +20,7 @@ contract ErosProposal{
 
     function Execute() external OnlyDAO returns(bool success){
         Executions++; //Updates first to avoid recursive calling
-        EROSDAO(DAO).ErosProposalExecuted(address(this));
+//        EROSDAO(DAO).ErosProposalExecuted(address(this));
 
         //External or internal code to execute
         ExtCon(ExternalContract).Increment();
