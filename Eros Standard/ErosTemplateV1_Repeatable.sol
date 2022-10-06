506771ce8afc89fd381a0c5c51e09a52a04a8639
Darius Tanz <scratchticketseb@gmail.com>
2022-10-06 13:17:30 +0000

    Oct 6, 2022, 9:17 AM

--- Eros Standard/ErosTemplateV1_Repeatable.sol
+++ Eros Standard/ErosTemplateV1_Repeatable.sol
@@ -33,7 +33,7 @@ contract ErosProposal{
         //External or internal code to execute
 
         if(address(this).balance > 0){
-            DAO.transfer(address(this).balance)
+            DAO.transfer(address(this).balance);
         }
         return(success);
     }
