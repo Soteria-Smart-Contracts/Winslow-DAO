79a4050d67cd584da91cee4b53a3adb592599e7e
Darius Tanz <scratchticketseb@gmail.com>
2022-10-06 16:57:44 +0000

    Oct 6, 2022, 12:57 PM

--- Core Contracts/Treasury/TreasuryV1.sol
+++ Core Contracts/Treasury/TreasuryV1.sol
@@ -28,3 +28,12 @@ contract HarmoniaDAO_V1_Treasury{
 
 
 }
+
+interface ERC20 {
+  function balanceOf(address owner) external view returns (uint256);
+  function allowance(address owner, address spender) external view returns (uint256);
+  function approve(address spender, uint value) external returns (bool);
+  function transfer(address to, uint value) external returns (bool);
+  function transferFrom(address from, address to, uint256 value) external returns (bool); 
+  function totalSupply() external view returns (uint);
+} 
