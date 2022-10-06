800e89f9e23c1130af75012ad1cbb37f5e0b1657
Darius Tanz <scratchticketseb@gmail.com>
2022-10-06 17:25:58 +0000

    Oct 6, 2022, 1:25 PM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.17;
 contract HarmoniaDAO_V1_Core{
     address public Treasury;
 
-    event FallbackToTreasury
+    event FallbackToTreasury(uint256 amount);
 
     function SetTreasury(address TreasuryAddress) external{
         Treasury = TreasuryAddress;
