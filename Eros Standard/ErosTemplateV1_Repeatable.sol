bfe2e46fd71ca447e0fda387129daf402209cf16
Darius Tanz <scratchticketseb@gmail.com>
2022-10-06 13:14:51 +0000

    Oct 6, 2022, 9:14 AM

--- Eros Standard/ErosTemplateV1_Repeatable.sol
+++ Eros Standard/ErosTemplateV1_Repeatable.sol
@@ -32,8 +32,7 @@ contract ErosProposal{
         ExtCon(ExternalContract).Increment();
         //External or internal code to execute
 
-
-
+        if()
         return(success);
     }
     //The contract will return any ether unused in the transaction back to the DAO on execution
