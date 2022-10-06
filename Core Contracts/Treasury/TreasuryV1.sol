04c9c6d017c3d4a1298a8afaa087839335c3e6b7
Darius Tanz <scratchticketseb@gmail.com>
2022-10-06 16:52:54 +0000

    Oct 6, 2022, 12:52 PM

--- Core Contracts/Treasury/TreasuryV1.sol
+++ Core Contracts/Treasury/TreasuryV1.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.17;
 
 contract HarmoniaDAO_V1_Treasury{
     address public DAO;
-    uint256 public Registered
+    uint256 public RegisteredAssetLimit = 5;
 
     constructor(address DAOcontract){
         DAO = DAOcontract;
