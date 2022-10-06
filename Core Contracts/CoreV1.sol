35a3fdf01d9ed969849cb9c9a1d7e2ae9ebbde5f
Darius Tanz <scratchticketseb@gmail.com>
2022-10-06 17:21:15 +0000

    Oct 6, 2022, 1:21 PM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -13,7 +13,7 @@ contract HarmoniaDAO_V1_Core{
 
     receive() external payable{
         //send ether to treasury then update balance there
-        Treasury.transfer()
+        Treasury.transfer(msg.value);
     }
 
     fallback() external payable{
