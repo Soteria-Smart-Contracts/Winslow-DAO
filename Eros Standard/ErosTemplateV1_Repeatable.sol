2f92e10dc295475277b9c9f89226f5267b83e19f
Darius Tanz <scratchticketseb@gmail.com>
2022-10-06 13:14:24 +0000

    Oct 6, 2022, 9:14 AM

--- Eros Standard/ErosTemplateV1_Repeatable.sol
+++ Eros Standard/ErosTemplateV1_Repeatable.sol
@@ -36,7 +36,7 @@ contract ErosProposal{
 
         return(success);
     }
-    //The contract will return any ether unused in the transaction back to the DAO on execution
+    //The contract will return any ether unused in the transaction back to the DAO on execution, so the contract will never have greater than 0 ether in state.
 
 
     //Additional functions can go here that can only be executed by the Execute() function
