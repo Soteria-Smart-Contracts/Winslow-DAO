fba92a6fc2a82be7785d6360a5c41fcc007e5f37
Darius Tanz <scratchticketseb@gmail.com>
2022-10-06 16:16:29 +0000

    Oct 6, 2022, 12:16 PM

--- Eros Standard/ErosTemplateV1.sol
+++ Eros Standard/ErosTemplateV1.sol
@@ -51,3 +51,11 @@ interface ExtCon{ //Interface name can be different, ensure it is updated correc
     function Update(string calldata) external;
 }
 
+interface ERC20 {
+  function balanceOf(address owner) external view returns (uint256);
+  function allowance(address owner, address spender) external view returns (uint256);
+  function approve(address spender, uint value) external returns (bool);
+  function transfer(address to, uint value) external returns (bool);
+  function transferFrom(address from, address to, uint256 value) external returns (bool); 
+  function totalSupply() external view returns (uint);
+} 
