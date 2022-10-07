48d4d0929b6ab6a863938f4e6b4fbfdb34ecf2a6
Darius Tanz <scratchticketseb@gmail.com>
2022-10-07 16:33:31 +0000

    Oct 7, 2022, 12:33 PM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -7,6 +7,12 @@ contract HarmoniaDAO_V1_Core{
 
     event FallbackToTreasury(uint256 amount);
 
+    modifier OnlyDAO{ //This same modifier must be used on external contracts called by this contract 
+        require(msg.sender == DAO);
+        _;
+    }
+
+
     function SetTreasury(address TreasuryAddress) external{
         Treasury = TreasuryAddress;
     }
