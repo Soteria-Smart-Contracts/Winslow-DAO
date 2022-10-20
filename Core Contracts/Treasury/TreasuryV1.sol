1ce73227678b9145988be28a3153e0f498b69d4b
Darius Tanz <scratchticketseb@gmail.com>
2022-10-20 18:28:29 +0000

    Oct 20, 2022, 2:28 PM

--- Core Contracts/Treasury/TreasuryV1.sol
+++ Core Contracts/Treasury/TreasuryV1.sol
@@ -52,7 +52,7 @@ contract HarmoniaDAOTreasury{
         RegisteredAssets[slot] =  Token(tokenAddress, ERC20(tokenAddress).balanceOf(address(this)));
         AssetRegistryMap[tokenAddress] = true;
 
-        emit AssetRegistered(NewToken, CurrentBalance);
+        emit AssetRegistered(Registe, CurrentBalance);
     }
 
     function UpdateERC20Balance(uint256 AssetID) internal {
