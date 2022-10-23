e4d28999fead64442fdfaa2410eb78188bd2279d
Darius Tanz <scratchticketseb@gmail.com>
2022-10-23 17:42:27 +0000

    Oct 23, 2022, 1:42 PM

--- Eros Standard/ErosTemplateV1.sol
+++ Eros Standard/ErosTemplateV1.sol
@@ -6,7 +6,7 @@ contract ErosProposal{
     address public DAO = 0x0000000000000000000000000000000000000000;
     bool public Executed;
 
-    //Fund request/s for the proposal, can only receive one asset per proposal, may receive both asset a
+    //Fund request/s for the proposal, can only receive one asset per proposal, may receive both ERC20 and Ether
     uint256 public RequestEther = 0 ether; //Optional, can be ommited
     uint256 public RequestTokens = 0; //Optional, can be ommited
     address TokenAddress = 0x0000000000000000000000000000000000000000; //Optional, can be ommited
