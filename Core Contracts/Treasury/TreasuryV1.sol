0a9bc9125903ac04da87563fa8b329b54552ba77
Darius Tanz <scratchticketseb@gmail.com>
2022-10-23 16:55:40 +0000

    Oct 23, 2022, 12:55 PM

--- Core Contracts/Treasury/TreasuryV1.sol
+++ Core Contracts/Treasury/TreasuryV1.sol
@@ -26,7 +26,7 @@ contract HarmoniaDAOTreasury{
     //Event Declarations
     event AssetRegistered(address NewToken, uint256 CurrentBalance);
     event AssetLimitChange(uint256 NewLimit);
-    event EtherReceived
+    event EtherReceived(uint256 amount, )
 
 
 
