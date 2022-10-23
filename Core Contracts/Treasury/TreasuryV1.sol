2251f4bc90894e495eac51f9d0dd35ec927a5994
Darius Tanz <scratchticketseb@gmail.com>
2022-10-23 18:21:37 +0000

    Oct 23, 2022, 2:21 PM

--- Core Contracts/Treasury/TreasuryV1.sol
+++ Core Contracts/Treasury/TreasuryV1.sol
@@ -28,7 +28,7 @@ contract HarmoniaDAOTreasury{
     event AssetLimitChange(uint256 NewLimit);
     event EtherReceived(uint256 amount, address Sender, address TxOrigin);
     event EtherSent(uint256 amount, address Receiver, address TxOrigin)
-    event ERC20BalanceUpdate(uint256 NewAmount)
+    event ERC20BalanceUpdate(uint256 NewAmount, address TxOrigin)
     //Code executed on deployment
     constructor(address DAOcontract, address CLDcontract){
         DAO = DAOcontract;
