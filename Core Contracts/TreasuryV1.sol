9c4c9272a0328468402770c42bc7d4b21147e79a
Darius Tanz <scratchticketseb@gmail.com>
2022-10-24 15:28:06 +0000

    Oct 24, 2022, 11:28 AM

--- Core Contracts/TreasuryV1.sol
+++ Core Contracts/TreasuryV1.sol
@@ -71,6 +71,7 @@ contract HarmoniaDAOTreasury{
                 uint256 DecimalReplacer = (10^10);
                 uint256 ToSend = ((CLDamount * ((AssetBalance * DecimalReplacer) / SupplyPreTransfer)) / DecimalReplacer);
                 ERC20(RegisteredAssets[CurrentID].TokenAddress).transfer(To, ToSend);
+                emit ERC20Sent(Amount, Receive, TxOrigin);
                 UpdateERC20Balance(CurrentID);
             }
             CurrentID++;
