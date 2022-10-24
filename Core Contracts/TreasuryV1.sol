d873bcbf832b2a22ad747e5dfdbe956a789b7ff8
Darius Tanz <scratchticketseb@gmail.com>
2022-10-24 14:04:12 +0000

    Oct 24, 2022, 10:04 AM

--- Core Contracts/TreasuryV1.sol
+++ Core Contracts/TreasuryV1.sol
@@ -63,7 +63,7 @@ contract HarmoniaDAOTreasury{
         require(ERC20(RegisteredAssets[0].TokenAddress).transferFrom(From, address(this), CLDamount), "Unable to transfer CLD to treasury, ensure allowance is given");
 
         uint8 CurrentID = 1;
-        while(CurrentID < RegisteredAssetLimit){ //It is very important that ERC20 contracts are audited properly to ensure that no errors could occur here, as one failed 
+        while(CurrentID < RegisteredAssetLimit){ //It is very important that ERC20 contracts are audited properly to ensure that no errors could occur here, as one failed transfer would revert the whole TX
             if(RegisteredAssets[CurrentID].TokenAddress != address(0)){ 
                 uint256 CutOfTotal = 
             }
