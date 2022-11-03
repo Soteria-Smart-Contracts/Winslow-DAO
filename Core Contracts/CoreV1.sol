dd5df04e6d8871fb725e76fa5b98b3fbaa843771
Darius Tanz <scratchticketseb@gmail.com>
2022-11-03 13:24:57 +0000

    Nov 3, 2022, 9:24 AM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -18,6 +18,7 @@ contract HarmoniaDAO_V1_Core{
         uint8 ProposalType; //Type 0 is simple ether and asset sends, Type 1 are Proxy Proposals for external governance, Type 2 are Eros Prosposals
         uint256 RequestedEtherAmount; //Optional, can be zero
         uint256 RequestedAssetAmount; //Optional, can be zero
+        
         bool Executed; //Can only be executed once, when finished, proposal exist only as archive
     }
 
