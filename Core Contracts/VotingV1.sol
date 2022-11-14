a9599ca91a22e45d6b707c8663e271d2968d0382
Darius Tanz <scratchticketseb@gmail.com>
2022-11-14 19:21:40 +0000

    Nov 14, 2022, 2:21 PM

--- Core Contracts/VotingV1.sol
+++ Core Contracts/VotingV1.sol
@@ -338,8 +338,8 @@ interface ERC20 {
   function Burn(uint256 _BurnAmount) external;
 }
 
-//Only for the first treasury, if the DAO contract is not updated but the treasury is in the future, only Eros proposals will be able to access it due to their flexibility
-interface TreasuryV1{//Only for the first treasury, if the DAO contract is not updated but the treasury is in the future,
+    //Only for the first treasury, if the DAO contract is not updated but the treasury is in the future, only Eros proposals will be able to access it due to their flexibility
+interface TreasuryV1{
     //Public State Modifing Functions
     function ReceiveRegisteredAsset(uint8 AssetID, uint amount) external;
     function UserAssetClaim(uint256 CLDamount) external returns(bool success);
