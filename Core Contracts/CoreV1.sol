61a2ea502655f7533d745bc9393e15b22c3c56f4
Darius Tanz <scratchticketseb@gmail.com>
2022-11-15 19:12:18 +0000

    Nov 15, 2022, 2:12 PM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -55,7 +55,7 @@ contract HarmoniaDAO_V1_Core{
 
     constructor(){
         TreasurySetter = msg.sender;
-        EmptyProxy = ProxyProposalArguments();
+        EmptyProxy = ProxyProposalArguments(0,0,0,address(0), );
     }
 
     //Public state-modifing functions
