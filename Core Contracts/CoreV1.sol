a11a90c481cf72e4c270e57a89f55dd61f028ddf
Darius Tanz <scratchticketseb@gmail.com>
2022-11-25 17:58:05 +0000

    Nov 25, 2022, 12:58 PM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -178,5 +178,5 @@ interface TreasuryV1{//Only for the first treasury, if the DAO contract is not u
 }
 
 interface EROS{
-    function DAO() external
+    function DAO() external view returns(address )
 }
