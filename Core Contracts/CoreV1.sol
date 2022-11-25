e41069d4696088b914b5f11f485c1ed53cbaa31a
Darius Tanz <scratchticketseb@gmail.com>
2022-11-25 17:25:52 +0000

    Nov 25, 2022, 12:25 PM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -99,8 +99,8 @@ contract HarmoniaDAO_V1_Core{
     //  Internal Executioning
     function InitializeSimpleProposal(uint256 NewIdentifier, address Slot, SimpleProposalTypes SimpleType, uint256 VotingLength, uint256 RequestedEther, uint256 RequestedAssetAmount, uint8 RequestedAssetID) internal returns(uint256 Identifier){
         require(VotingLength >= 86400);
-        if(SimpleType != )
-        
+        if(SimpleType != SimpleProposalTypes())
+
         Proposal memory NewProposal = Proposal(NewIdentifier, ProposalStatus(0), SecurityStatus(0), ProposalTypes(0), SimpleProposalTypes(SimpleType), VotingLength, RequestedEther, RequestedAssetAmount, RequestedAssetID, EmptyProxy, false);
         Proposals.push(NewProposal);
 
