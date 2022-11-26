8a8b27d439dea8fbbf494c524c19d6e081e539e6
Darius Tanz <scratchticketseb@gmail.com>
2022-11-26 19:29:38 +0000

    Nov 26, 2022, 2:29 PM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -86,7 +86,7 @@ contract HarmoniaDAO_V1_Core{
     //Public state-modifing functions
 
     function SubmitSimpleProposal(address Slot, SimpleProposalTypes SimpleType, uint256 VotingLength, uint256 RequestedEther, uint256 RequestedAssetAmount, uint8 RequestedAssetID) public returns(bool success, uint256 Identifier){
-        uint256 NewIdentifier
+        uint256 NewIdentifier = Proposals.length
         InitializeSimpleProposal()
     }
 
