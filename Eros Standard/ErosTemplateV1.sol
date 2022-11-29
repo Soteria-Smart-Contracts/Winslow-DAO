4cf40596fb59df6bc0748ba53e363015f26614e7
Darius Tanz <scratchticketseb@gmail.com>
2022-11-29 18:59:39 +0000

    Nov 29, 2022, 1:59 PM

--- Eros Standard/ErosTemplateV1.sol
+++ Eros Standard/ErosTemplateV1.sol
@@ -50,6 +50,7 @@ contract ErosProposal{
 interface HarmoniaDAO{
     function CheckErosApproval(address) external view returns(bool);
     function ErosProposalExecuted(address) external; //This function in the DAO must require that the msg.sender is the same as the input
+    function Treasury
 }
 
 //Only for the first treasury, if the DAO contract is not updated but the treasury is in the future, only Eros proposals will be able to access it due to their flexibility
