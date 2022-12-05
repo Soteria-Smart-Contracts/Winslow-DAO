cc971dc12107cd2b4b44c92baa84e077f7287e26
Darius Tanz <scratchticketseb@gmail.com>
2022-12-05 18:16:13 +0000

    Dec 5, 2022, 1:16 PM

--- Core Contracts/VotingV1.sol
+++ Core Contracts/VotingV1.sol
@@ -38,8 +38,8 @@ contract VotingSystemV1 {
         uint256 VoteEnds;    //Unix Time
         VoteResult Result;   //Using VoteResult enum
         uint256 ActiveVoters;//Total Number of users that have voted
-        uint256 YEAvotes;    //Votes Appro
-        uint256 NAYvotes;
+        uint256 YEAvotes;    //Votes to approve
+        uint256 NAYvotes;    //Votes to refuse
         bool Executed;
         uint256 TotalIncentive;
         uint256 IncentivePerVote;
