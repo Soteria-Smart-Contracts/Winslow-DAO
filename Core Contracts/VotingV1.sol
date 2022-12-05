aa319c9b3723e3222fbfe422fec4a9de8cb87f82
Darius Tanz <scratchticketseb@gmail.com>
2022-12-05 18:13:16 +0000

    Dec 5, 2022, 1:13 PM

--- Core Contracts/VotingV1.sol
+++ Core Contracts/VotingV1.sol
@@ -85,9 +85,6 @@ contract VotingSystemV1 {
 
     function IncentivizeProposal(uint256 proposalId, uint256 amount) external {
         require(ERC20(CLD).transferFrom(msg.sender, address(this), amount), "VotingSystemV1.IncentivizeProposal: You do not have enough CLD to incentivize this proposal or you may not have given this contract enough allowance");
-//        require(ERC20(CLD).allowance(msg.sender, address(this)) >= amount, 
-//            "VotingSystemV1.IncentivizeProposal: You have not given Voting enough allowance" //Dont need this, transferfrom will fail first
-//        );
         require(VotingInstances[proposalId].Result == VoteResult(0), 
             'VotingSystemV1.IncentivizeProposal: This proposal has ended');
         require(block.timestamp <= VotingInstances[proposalId].VoteEnds, "VotingSystemV1.IncentivizeProposal: The voting period has ended, save for the next proposal!");
