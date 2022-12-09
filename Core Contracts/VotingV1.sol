73df4c2f4fe41eb830e584479004a90482b75056
Darius Tanz <scratchticketseb@gmail.com>
2022-12-09 14:01:42 +0000

    Dec 9, 2022, 9:01 AM

--- Core Contracts/VotingV1.sol
+++ Core Contracts/VotingV1.sol
@@ -192,10 +192,8 @@ contract VotingSystemV1 {
     /////////////////////////////////////////
 
     // TO DO Refactor this
-    function _returnTokens(uint256 _proposalId,address _voterAddr)
-        internal {
-        require(block.timestamp >= VotingInstances[_proposalId].VoteEnds, 
-            "VotingSystemV1.WithdrawMyTokens: The voting period hasn't ended");
+    function _returnTokens(uint256 _proposalId,address _voterAddr) internal {
+        require(block.timestamp >= VotingInstances[_proposalId].VoteEnds, "VotingSystemV1.WithdrawMyTokens: The voting period hasn't ended");
 
         if (VotingInstances[_proposalId].ActiveVoters > 0) {
             require(
