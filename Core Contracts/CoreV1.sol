bf733c07b730e9018abec0a697a96de13859d000
Darius Tanz <scratchticketseb@gmail.com>
2022-12-09 13:44:55 +0000

    Dec 9, 2022, 8:44 AM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -47,6 +47,7 @@ contract HarmoniaDAO_V1_Core{
     struct Proposal{
         uint256 ProposalID;
         address AddressSlot; //To set an address either as a receiver, ProxyReceiver for approval of Eros proposal contract
+        string Memo;
         ProposalStatus Status; //Types declared in enum
         SecurityStatus SecurityLevel; //Types declared in enum
         ProposalTypes ProposalType; //Types declared in enum
