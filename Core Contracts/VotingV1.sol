7ed15381ecead967e1f765cbcbed506791373f8d
Darius Tanz <scratchticketseb@gmail.com>
2022-12-09 14:41:40 +0000

    Dec 9, 2022, 9:41 AM

--- Core Contracts/VotingV1.sol
+++ Core Contracts/VotingV1.sol
@@ -87,8 +87,7 @@ contract VotingSystemV1 {
     }//Checked
 
     function CastVote(uint256 amount, uint256 VotingInstance, Vote VoteChoice) external {
-        require( ERC20(CLD).transferFrom(msg.sender, address(this), amount), 
-            "VotingSystemV1.CastVote: You do not have enough CLD to vote this amount or have not given the proper allowance to voting");
+        require(ERC20(CLD).transferFrom(msg.sender, address(this), amount), "VotingSystemV1.CastVote: You do not have enough CLD to vote this amount or have not given the proper allowance to voting");
         require(
             VoteChoice == Vote(0) || VoteChoice == Vote(1), 
             "VotingSystemV1.CastVote: You must either vote 'Yea' or 'Nay'"
