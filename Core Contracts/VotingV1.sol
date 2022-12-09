cb5ec3b072082fc47fae9431e1ed7645b2089d1c
Darius Tanz <scratchticketseb@gmail.com>
2022-12-09 15:58:17 +0000

    Dec 9, 2022, 10:58 AM

--- Core Contracts/VotingV1.sol
+++ Core Contracts/VotingV1.sol
@@ -143,7 +143,7 @@ contract VotingSystemV1 {
     }
 
     function SetTaxAmount(uint256 NewExecCut, uint256 NewBurnCut) public OnlyDAO returns (bool success) {
-        
+        require(condition);
 
         return true;
     }
