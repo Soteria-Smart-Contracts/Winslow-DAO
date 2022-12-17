5008c35916731d3b1216bb1b0984229c1b45321b
Darius Tanz <scratchticketseb@gmail.com>
2022-12-17 16:00:27 +0000

    Dec 17, 2022, 11:00 AM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -103,8 +103,8 @@ contract HarmoniaDAO_V1_Core{
     }
 
 
-//    function SubmitErosProposal(address Slot, ) public returns(bool success, uint256 Identifier){
-//    }
+    function SubmitErosProposal(address Slot, ) public returns(bool success, uint256 Identifier){}
+    }
 
 
     //  Public view functions
