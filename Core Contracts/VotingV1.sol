a202449ed5906b78944e39f34384671dcfc762e2
Darius Tanz <scratchticketseb@gmail.com>
2022-12-18 19:04:35 +0000

    Dec 18, 2022, 2:04 PM

--- Core Contracts/VotingV1.sol
+++ Core Contracts/VotingV1.sol
@@ -1,6 +1,6 @@
 //SPDX-License-Identifier:UNLICENSE
 //This contract is able to be replaced by the Harmonia Core, and can also continue to be used if a new Harmonia Core is deployed by changing DAO addresses
-//When setting up a new core or voting contract, ensure cross-compatibility and record keeping is either done by the archive contract,
+//When setting up a new core or voting contract, ensure cross-compatibility and record keeping done by the archive contract, 
 pragma solidity ^0.8.17;
 
 
