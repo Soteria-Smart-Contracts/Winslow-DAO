24ccf6e07c44f5ef0d4f0e6947cf669baba9f6ee
Darius Tanz <scratchticketseb@gmail.com>
2022-12-18 19:09:20 +0000

    Dec 18, 2022, 2:09 PM

--- Core Contracts/VotingV1.sol
+++ Core Contracts/VotingV1.sol
@@ -115,7 +115,7 @@ contract VotingSystemV1 {
 
     function ReturnTokens(uint256 VotingInstance) external { //For returning your tokens for a specific instance after voting, with the incentive payout
         require(VoterInfo[VotingInstance][msg.sender].Voted = true);
-        require(VoterInfo[VotingInstance][msg.sender].);
+        require(VoterInfo[VotingInstance][msg.sender].TokensReturns = false);
 
         uint256 TotalToReturn;
         TotalToReturn += VoterInfo[VotingInstance][msg.sender].VotesLocked;
