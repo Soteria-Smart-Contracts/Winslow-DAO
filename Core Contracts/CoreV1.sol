9db870747aec0896e5e7b179f21ccce3bc429ad9
Darius Tanz <scratchticketseb@gmail.com>
2022-12-18 17:11:56 +0000

    Dec 18, 2022, 12:11 PM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -46,8 +46,8 @@ contract HarmoniaDAO_V1_Core{
 
     struct Proposal{
         uint256 ProposalID;
-        address AddressSlot; //To set an address either as a receiver, ProxyReceiver for approval of Eros proposal contract
-        string Memo; //Short description of what the proposal is and does (Reduce length for gas efficiency)
+        address AddressSlot;            //To set an address either as a receiver, ProxyReceiver for approval of Eros proposal contract
+        string Memo;                    //Short description of what the proposal is and does (Reduce length for gas efficiency)
         ProposalStatus Status; //Types declared in enum
         SecurityStatus SecurityLevel; //Types declared in enum
         ProposalTypes ProposalType; //Types declared in enum
@@ -55,7 +55,7 @@ contract HarmoniaDAO_V1_Core{
         uint256 ProposalVotingLenght; //Minimum 24 hours
         uint256 RequestedEtherAmount; //Optional, can be zero
         uint256 RequestedAssetAmount; //Optional, can be zero
-        uint8 RequestedAssetID;
+        uint8 RequestedAssetID;       
         ProxyProposalArguments ProxyArgs;
         bool Executed; //Can only be executed once, when finished, proposal exist only as archive
     }
