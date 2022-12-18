029b80a4c0568eba158de8775670d095225da47d
Darius Tanz <scratchticketseb@gmail.com>
2022-12-18 18:56:13 +0000

    Dec 18, 2022, 1:56 PM

--- Core Contracts/VotingV1.sol
+++ Core Contracts/VotingV1.sol
@@ -69,6 +69,8 @@ contract VotingSystemV1 {
         CLD = CLDAddr;
     }
 
+
+    //
     function IncentivizeProposal(uint256 VotingInstance, uint256 amount) public returns(bool success){
         require(ERC20(CLD).transferFrom(msg.sender, address(this), amount), "VotingSystemV1.IncentivizeProposal: You do not have enough CLD to incentivize this proposal or you may not have given this contract enough allowance");
         require(VotingInstances[VotingInstance].Status == VoteStatus(0), 'VotingSystemV1.IncentivizeProposal: This proposal has ended');
