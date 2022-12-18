86f980e7af964de86fe8875ed4a907dc6b1180f4
Darius Tanz <scratchticketseb@gmail.com>
2022-12-18 19:07:21 +0000

    Dec 18, 2022, 2:07 PM

--- Core Contracts/VotingV1.sol
+++ Core Contracts/VotingV1.sol
@@ -91,7 +91,7 @@ contract VotingSystemV1 {
     function CastVote(uint256 amount, uint256 VotingInstance, Vote VoteChoice) external returns(bool success){
         require(ERC20(CLD).transferFrom(msg.sender, address(this), amount), "VotingSystemV1.CastVote: You do not have enough CLD to vote this amount or have not given the proper allowance to voting");
         require(VoteChoice == Vote(0) || VoteChoice == Vote(1), "VotingSystemV1.CastVote: You must either vote YEA or NAY");
-        require(!VoterInfo[VotingInstance][msg.sender].Voted, "VotingSystemV1.CastVote: You already voted in this proposal");
+        require(!VoterInfo[VotingInstance][msg.sender].Voted, "VotingSystemV1.CastVote: You may only castl");
         require(block.timestamp >= VotingInstances[VotingInstance].VoteStarts && block.timestamp <= VotingInstances[VotingInstance].VoteEnds, "VotingSystemV1.CastVote: This instance is not currently in voting");
 
         if(VoteChoice == Vote(0)) {
