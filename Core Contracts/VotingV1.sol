60a2dfe74de32a36a99910e85516f86c9257e939
Darius Tanz <scratchticketseb@gmail.com>
2022-12-18 18:50:14 +0000

    Dec 18, 2022, 1:50 PM

--- Core Contracts/VotingV1.sol
+++ Core Contracts/VotingV1.sol
@@ -103,7 +103,7 @@ contract VotingSystemV1 {
     }
 
     function WithdrawVoteTokens(uint256 VotingInstance) external { //Seb review this it looks weird
-        require(VoterInfo[VotingInstance][msg.sender].);
+        require(VoterInfo[VotingInstance][msg.sender].VotesLocked > );
 
         //emit IncentiveWithdrawed(VotingInstances[VotingInstance].IncentiveAmount);
     }
