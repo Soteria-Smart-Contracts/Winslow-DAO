7eb4904d6c88fbaf316cbc3505f9e51e822f6730
Darius Tanz <scratchticketseb@gmail.com>
2022-12-19 13:35:38 +0000

    Dec 19, 2022, 8:35 AM

--- Core Contracts/VotingV1.sol
+++ Core Contracts/VotingV1.sol
@@ -148,7 +148,7 @@ contract VotingSystemV1 {
         
         ERC20(CLD).transfer(msg.sender, VotingInstances[VotingInstance].CLDToExecutioner);
 
-        
+        Inv
 
         VotingInstances[VotingInstance].Status = VoteStatus(2);
         //Post results
