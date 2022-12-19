235e99d666526192ad72e7908c8ca2b57c3ca36f
Darius Tanz <scratchticketseb@gmail.com>
2022-12-19 18:05:45 +0000

    Dec 19, 2022, 1:05 PM

--- Core Contracts/VotingV1.sol
+++ Core Contracts/VotingV1.sol
@@ -16,7 +16,7 @@ contract VotingSystemV1 {
 
     // Proposals being tracked by id here
     VoteInstance[] public VotingInstances;
-    mapping(uint256 => )
+    mapping(uint256 => MultiOptionVotes) public 
     // Map user addresses over their info
     mapping (uint256 => mapping (address => VoterDetails)) public VoterInfo;
 
