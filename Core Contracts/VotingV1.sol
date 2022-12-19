87af5c7108d40addce09dbcbb431f9072e262ca9
Darius Tanz <scratchticketseb@gmail.com>
2022-12-19 13:50:13 +0000

    Dec 19, 2022, 8:50 AM

--- Core Contracts/VotingV1.sol
+++ Core Contracts/VotingV1.sol
@@ -123,7 +123,7 @@ contract VotingSystemV1 {
 
         uint256 TotalToReturn;
         TotalToReturn += VoterInfo[VotingInstance][msg.sender].VotesLocked;
-       // TotalToReturn += 
+        TotalToReturn += 
 
         //emit IncentiveWithdrawed(VotingInstances[VotingInstance].IncentiveAmount);
     }
