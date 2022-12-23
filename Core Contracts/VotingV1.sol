0da7e45d549d9b39eb2b3de768c2f18b611b5d93
Darius Tanz <scratchticketseb@gmail.com>
2022-12-23 21:56:46 +0000

    Dec 23, 2022, 4:56 PM

--- Core Contracts/VotingV1.sol
+++ Core Contracts/VotingV1.sol
@@ -150,7 +150,7 @@ contract VotingSystemV1 {
 
         ERC20(CLD).transfer(msg.sender, TotalToReturn);
 
-        //emit TokensReturned(msg.sender, TotalToReturn, (TotalToReturn - VoterInfo[VotingInstance][msg.sender].VotesLocked));
+        emit TokensReturned(msg.sender, TotalToReturn, (TotalToReturn - VoterInfo[VotingInstance][msg.sender].VotesLocked));
     }
 
     //OnlyDAO functions
