d0f8c6f62feda29dfa9e3ea38aa9bc4150f3d164
Darius Tanz <scratchticketseb@gmail.com>
2022-12-27 22:37:51 +0000

    Dec 27, 2022, 5:37 PM

--- Core Contracts/VotingV1.sol
+++ Core Contracts/VotingV1.sol
@@ -128,8 +128,8 @@ contract VotingSystemV1 {
         require(ERC20(CLD).transferFrom(msg.sender, address(this), amount), "VotingSystemV1.CastMultiVote: You do not have enough CLD to vote this amount or have not given the proper allowance to voting");
         require(VoteChoice == Vote(0) || VoteChoice == Vote(1), "VotingSystemV1.CastMultiVote: You must either vote YEA or NAY");
         require(amount >= 10000000000000000, "VotingSystemV1.CastMultiVote: The minimum CLD per vote is 0.01"); //For incentive payout reasons
-        require(!VoterInfo[VotingInstance][msg.sender].Voted, "VotingSystemV1.CasCastMultiVotetVote: You may only cast a single vote per address per proposal"); //This may be changed in V2
-        require(block.timestamp >= VotingInstances[VotingInstance].VoteStarts && block.timestamp <= VotingInstances[VotingInstance].VoteEnds, "VotingSystemV1.CastVote: This instance is not currently in voting");
+        require(!VoterInfo[VotingInstance][msg.sender].Voted, "VotingSystemV1.CastMultiVote: You may only cast a single vote per address per proposal"); //This may be changed in V2
+        require(block.timestamp >= VotingInstances[VotingInstance].VoteStarts && block.timestamp <= VotingInstances[VotingInstance].VoteEnds, "VotingSystemV1.CastMultiVote: This instance is not currently in voting");
 
 
         
