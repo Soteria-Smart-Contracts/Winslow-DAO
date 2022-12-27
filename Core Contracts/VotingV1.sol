5ea455bd732dcac4d2dab2bb7a769de16540ecd5
Darius Tanz <scratchticketseb@gmail.com>
2022-12-27 19:17:51 +0000

    Dec 27, 2022, 2:17 PM

--- Core Contracts/VotingV1.sol
+++ Core Contracts/VotingV1.sol
@@ -26,7 +26,7 @@ contract VotingSystemV1 {
         uint256 VoteStarts;      //Unix Time
         uint256 VoteEnds;        //Unix Time
         VoteStatus Status;       //Using VoteStatus enum
-        uint256 ActiveVoters;    //Total Number of users that have voted
+        uint256 Voters;    //Total Number of users that have voted
         uint256 TotalCLDVoted;   //Total of CLD used in this instance for voting
         bool MultiVote;          //Determines if this instance supports multivote
         uint256 YEAvotes;        //Votes to approve
