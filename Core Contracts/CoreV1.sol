06693f15e1aefdc67a05a8e1073dd059584b7c49
Darius Tanz <scratchticketseb@gmail.com>
2022-12-28 16:09:50 +0000

    Dec 28, 2022, 11:09 AM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -132,7 +132,7 @@ contract Winslow_Core_V1{
     }
 
     function InitializeProxyProposal(uint256 NewIdentifier, address Slot, string memory Memo, uint256 VotingLength, uint256 RequestedEther, uint256 RequestedAssetAmount, uint8 RequestedAssetID, ProxyProposalArguments memory ProxyArgs) internal returns(uint256 identifier){
-        
+
         require(VotingLength >= 86400 && VotingLength <= 1209600, "Voting must be atleast 24 hours and less than two weeks");
         require(Slot != address(0), "ProxyProposals must be a contract");
 
@@ -140,9 +140,11 @@ contract Winslow_Core_V1{
         Proposals.push(NewProposal);
 
         return(NewIdentifier);
+
     }
 
     function InitializeErosProposal(uint256 NewIdentifier, address Slot, string memory Memo, uint256 VotingLength, uint256 RequestedEther, uint256 RequestedAssetAmount, uint8 RequestedAssetID) internal returns(uint256 identifier){
+
         require(VotingLength >= 86400 && VotingLength <= 1209600, "Voting must be atleast 24 hours and less than two weeks");
         require(Slot != address(0), "ErosProposals must be a contract");
 
@@ -150,6 +152,7 @@ contract Winslow_Core_V1{
         Proposals.push(NewProposal);
 
         return(NewIdentifier);
+        
     }
 
     //    function RegisterTreasuryAsset(address tokenAddress, uint8 slot, uint256 ProposalID) internal returns(bool success){
