831130f52a6fbd5fff97b206a43a8b034dbdba0e
Darius Tanz <scratchticketseb@gmail.com>
2022-12-30 15:10:51 +0000

    Dec 30, 2022, 10:10 AM

--- Core Contracts/VotingV1.sol
+++ Core Contracts/VotingV1.sol
@@ -240,12 +240,12 @@ contract Winslow_Voting_V1 {
 
     receive() external payable{
         emit FallbackToTreasury(address(this).balance);
-        payable(WinslowDAOV1(DAO).Treasury()).transfer(address(this).balance);
+        payable(CoreV1(DAO).Treasury()).transfer(address(this).balance);
     }
 
     fallback() external payable{
         emit FallbackToTreasury(address(this).balance);
-        payable(WinslowDAOV1(DAO).Treasury()).transfer(address(this).balance);
+        payable(CoreV1(DAO).Treasury()).transfer(address(this).balance);
     }
 
 }
@@ -253,7 +253,7 @@ contract Winslow_Voting_V1 {
 
 //Interfaces
 
-interface WinslowDAOV1 {
+interface CoreV1 {
     function Treasury() external returns(address payable TreasuryAddress);
 }
 
