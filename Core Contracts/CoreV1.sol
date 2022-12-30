8dd1c755d6420ac079abdade6f435213fe8407a6
Darius Tanz <scratchticketseb@gmail.com>
2022-12-30 16:44:29 +0000

    Dec 30, 2022, 11:44 AM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -89,6 +89,7 @@ contract Winslow_Core_V1{
 
     function SubmitSimpleProposal(address Slot, SimpleProposalTypes SimpleType, string memory Memo, uint256 VotingLength, uint256 RequestedEther, uint256 RequestedAssetAmount, uint8 RequestedAssetID) public returns(bool success, uint256 Identifier){
 
+        require(ReceiveProposalCost());
 
         uint256 NewIdentifier = MRIdentifier++;
         InitializeSimpleProposal(NewIdentifier, Slot, Memo, SimpleType, VotingLength, RequestedEther, RequestedAssetAmount, RequestedAssetID);
@@ -112,6 +113,8 @@ contract Winslow_Core_V1{
 
 
     function SubmitErosProposal(address Slot, string memory Memo, uint256 VotingLength, uint256 RequestedEther, uint256 RequestedAssetAmount, uint8 RequestedAssetID) public returns(bool success, uint256 Identifier){
+
+        require(ReceiveProposalCost());
 
         uint256 NewIdentifier = MRIdentifier++;
         InitializeErosProposal(NewIdentifier, Slot, Memo, VotingLength, RequestedEther, RequestedAssetAmount, RequestedAssetID);
