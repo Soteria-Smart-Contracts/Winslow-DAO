74c568967fec6d6d306be8911bc2e849d5cc7193
Darius Tanz <scratchticketseb@gmail.com>
2022-12-30 17:48:08 +0000

    Dec 30, 2022, 12:48 PM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -8,7 +8,7 @@ contract Winslow_Core_V1{
     address public Treasury = address(0);
     address public InitialSetter;
     bool public InitialTreasurySet = false;
-    uint256 public ProposalCost = 100000000000000000000;
+    uint256 public ProposalCost = 100000000000000000000; //Initial cost, can be changed via proposals
     ProxyProposalArguments internal EmptyProxy;
 
     //Mapping, structs and other declarations
