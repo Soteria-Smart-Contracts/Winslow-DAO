5cb402ca6a28e043ecac9b46fdddd90a864677e0
Darius Tanz <scratchticketseb@gmail.com>
2022-12-31 03:04:00 +0000

    Dec 30, 2022, 10:04 PM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -145,7 +145,7 @@ contract Winslow_Core_V1{
         MRIdentifier++;
 
         //All simple proposals must have a slotted address for sending or action, but may be 0 in certain cases such as burn events
-        VotingV1(Voting).InitializeVoteInstance(NewIdentifier, VotingLength, false);
+        VotingInstanceID = VotingV1(Voting).InitializeVoteInstance(NewIdentifier, VotingLength, false);
         Proposals[NewIdentifier] = Proposal(NewIdentifier, Slot, Memo, ProposalStatus(0), SecurityStatus(0), ProposalTypes(0), SimpleType, VotingLength, RequestedEther, RequestedAssetAmount, RequestedAssetID, EmptyProxy, false, msg.sender);
 
         return(NewIdentifier);
