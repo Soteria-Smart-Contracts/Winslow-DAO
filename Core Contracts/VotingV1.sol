6522b357073d999a71bac7b5911bb255d471c25b
Darius Tanz <scratchticketseb@gmail.com>
2022-12-31 02:33:12 +0000

    Dec 30, 2022, 9:33 PM

--- Core Contracts/VotingV1.sol
+++ Core Contracts/VotingV1.sol
@@ -140,7 +140,7 @@ contract Winslow_Voting_V1 {
             emit VoteCast(msg.sender, VotingInstance, "NEA", amount);
         }
 
-        if OptionChoice
+        if (OptionChoice == )
 
         MultiVotes[VotingInstance].OptionOne = 2;
         
