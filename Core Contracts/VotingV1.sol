0913a2217993f7408060496756aa458118bf65e4
Darius Tanz <scratchticketseb@gmail.com>
2022-12-31 02:21:53 +0000

    Dec 30, 2022, 9:21 PM

--- Core Contracts/VotingV1.sol
+++ Core Contracts/VotingV1.sol
@@ -125,7 +125,7 @@ contract Winslow_Voting_V1 {
         //This is set up so that you can vote for or against the proposal, and if yes what of the options you prefer
     function CastMultiVote(uint256 amount, uint256 VotingInstance, Vote VoteChoice, MultiOptions OptionChoice) external returns(bool success){ 
 
-        require(VotingInstances[VotingInstance].);
+        require(VotingInstances[VotingInstance].MultiVote == false);
         require(ERC20(CLDAddress()).transferFrom(msg.sender, address(this), amount), "VotingSystemV1.CastMultiVote: You do not have enough CLD to vote this amount or have not given the proper allowance to voting");
         require(VoteChoice == Vote(0) || VoteChoice == Vote(1), "VotingSystemV1.CastMultiVote: You must either vote YEA or NAY");
         require(amount >= 10000000000000000, "VotingSystemV1.CastMultiVote: The minimum CLD per vote is 0.01"); //For incentive payout reasons
