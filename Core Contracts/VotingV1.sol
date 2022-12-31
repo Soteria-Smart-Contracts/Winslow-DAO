2f56cfad90734787c3152c258afa5cf10bb9c770
Darius Tanz <scratchticketseb@gmail.com>
2022-12-31 02:30:46 +0000

    Dec 30, 2022, 9:30 PM

--- Core Contracts/VotingV1.sol
+++ Core Contracts/VotingV1.sol
@@ -73,6 +73,8 @@ contract Winslow_Voting_V1 {
         VotingComplete
     }
 
+    //TODO: Review events for each function
+
     event ProposalCreated(address proposer, uint256 proposalID, uint256 voteStart, uint256 voteEnd);
     event ProposalPassed(address executor, uint256 VotingInstance, uint256 amountBurned, uint256 executShare);
     event ProposalNotPassed(address executor, uint256 VotingInstance, uint256 amountBurned, uint256 executShare);
