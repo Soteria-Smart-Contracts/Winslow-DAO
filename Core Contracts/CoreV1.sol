ec8f49dd81812a432ed62c1b036408f271fe6e75
Darius Tanz <scratchticketseb@gmail.com>
2023-01-05 21:39:48 +0000

    Jan 5, 2023, 4:39 PM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -242,6 +242,10 @@ interface VotingV1{
 
 }
 
+interface CoreReplacement{
+    function 
+}
+
 //Only for the first treasury, if the DAO contract is not updated but the treasury is in the future, only Eros proposals will be able to access it due to their flexibility
 interface TreasuryV1 {
 //Public State Modifing Functions
