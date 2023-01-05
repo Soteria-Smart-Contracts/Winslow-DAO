eb7ed3fe7d8ec084fdbe43011fe6307ef2ce44e8
Darius Tanz <scratchticketseb@gmail.com>
2023-01-05 21:48:14 +0000

    Jan 5, 2023, 4:48 PM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -243,7 +243,9 @@ interface VotingV1{
 }
 
 interface CoreReplacement{
-    function InheritCore(address Treasury, address Voting, uint256 LatestProposal, uint256 ProposalCost)
+    function InheritCore(address Treasury, address Voting, uint256 LatestProposal, uint256 ProposalCost) external returns(bool success){
+        
+    }
 }
 
 //Only for the first treasury, if the DAO contract is not updated but the treasury is in the future, only Eros proposals will be able to access it due to their flexibility
