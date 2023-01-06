bd741b3aeaa7b93216cc18aa2fac13db1c4a8439
Darius Tanz <scratchticketseb@gmail.com>
2023-01-06 15:02:24 +0000

    Jan 6, 2023, 10:02 AM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -141,7 +141,7 @@ contract Winslow_Core_V1{
 
         if(SimpleType == SimpleProposalTypes(2)){
             require(UintSlot > 0);
-            
+
         }
 
         uint256 NewIdentifier = MRIdentifier++;
@@ -149,6 +149,10 @@ contract Winslow_Core_V1{
 
         //All simple proposals must have a slotted address for sending or action, but may be 0 in certain cases such as burn events
         uint256 VotingInstanceID = Voting(VotingContract).InitializeVoteInstance(NewIdentifier, VotingLength, false);
+        if(SimpleType == SimpleProposalTypes(2)){
+            require(UintSlot > 0);
+            
+        }
         Proposals[NewIdentifier] = Proposal(NewIdentifier, AddressSlot, Memo, ProposalStatus(0), SecurityStatus(0), ProposalTypes(0), SimpleType, VotingInstanceID, VotingLength, RequestedEther, RequestedAssetAmount, RequestedAssetID, EmptyProxy, false, msg.sender);
 
         return(NewIdentifier);
