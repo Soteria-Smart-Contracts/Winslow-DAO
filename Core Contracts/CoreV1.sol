85c290ff97c4249e4db12dd41a17e02901c8f8be
Darius Tanz <scratchticketseb@gmail.com>
2023-01-06 18:30:23 +0000

    Jan 6, 2023, 1:30 PM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -199,7 +199,7 @@ contract Winslow_Core_V1{
         // AssetSend
     function SendAssets(uint256 ProposalID) internal returns(bool success){
 
-        Treasury(TreasuryContract).
+        Treasury(TreasuryContract).TransferETH(, receiver);
     }
     
         // AssetRegister
