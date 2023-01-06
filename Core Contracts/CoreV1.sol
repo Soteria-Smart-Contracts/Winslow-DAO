050b70640e0b8af88dd83bc89ec04a4e9ee107cd
Darius Tanz <scratchticketseb@gmail.com>
2023-01-06 14:54:47 +0000

    Jan 6, 2023, 9:54 AM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -135,7 +135,7 @@ contract Winslow_Core_V1{
 
     //  Internal Functions
 
-    function InitializeSimpleProposal( address Slot, string memory Memo, SimpleProposalTypes SimpleType, uint256 VotingLength, uint256 RequestedEther, uint256 RequestedAssetAmount, uint8 RequestedAssetID) internal returns(uint256 Identifier){
+    function InitializeSimpleProposal(address Slot, string memory Memo, SimpleProposalTypes SimpleType, uint256 VotingLength, uint256 RequestedEther, uint256 RequestedAssetAmount, uint8 RequestedAssetID) internal returns(uint256 Identifier){
 
         require(VotingLength >= 86400 && VotingLength <= 1209600, "Voting must be atleast 24 hours and less than two weeks");
 
