35798a2dedecd00b72ef9fb04559aaa12d925f6b
Darius Tanz <scratchticketseb@gmail.com>
2023-01-06 15:12:11 +0000

    Jan 6, 2023, 10:12 AM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -303,6 +303,7 @@ interface Treasury {
     function ChangeRegisteredAssetLimit(uint8 NewLimit) external;
 //Public View Functions
     function CLDAddress() external view returns(address CLD);
+    function RegisteredAssetLimit()
     function IsRegistered(address TokenAddress) external view returns(bool);
     function GetBackingValueEther(uint256 CLDamount) external view returns(uint256 EtherBacking);
     function GetBackingValueAsset(uint256 CLDamount, uint8 AssetID) external view returns(uint256 AssetBacking);
