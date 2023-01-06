768503bcc2f257262746e96a7ba3ffd2250c0e54
Darius Tanz <scratchticketseb@gmail.com>
2023-01-06 15:01:50 +0000

    Jan 6, 2023, 10:01 AM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -140,7 +140,8 @@ contract Winslow_Core_V1{
         require(VotingLength >= 86400 && VotingLength <= 1209600, "Voting must be atleast 24 hours and less than two weeks");
 
         if(SimpleType == SimpleProposalTypes(2)){
-            require(condition);
+            require(UintSlot > 0);
+            
         }
 
         uint256 NewIdentifier = MRIdentifier++;
