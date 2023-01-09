740687a7007ea9209e9719decb8c8c833021a1aa
Darius Tanz <scratchticketseb@gmail.com>
2023-01-09 19:46:05 +0000

    Jan 9, 2023, 2:46 PM

--- Archive/SaleModuleV1.sol
+++ Archive/SaleModuleV1.sol
@@ -2,7 +2,7 @@
 /* This contract is able to be replaced by the Winslow Core, and can also continue to be used 
 if a new Winslow Core is deployed by changing DAO addresses
 When setting up a new core or voting contract, ensure cross-compatibility and record keeping 
-done by the archive contract, voting index and proposal indexes never restart */
+done by the archive contract, voting index and proposal indexes never restart */TODO:Edit this
 pragma solidity ^0.8.17;
 
 contract CLDAuctionFactory {
