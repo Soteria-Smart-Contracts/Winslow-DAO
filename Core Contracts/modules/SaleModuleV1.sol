ef469e4c7876b67711af182b68a7783741b654e4
Darius Tanz <scratchticketseb@gmail.com>
2023-01-09 16:15:01 +0000

    Jan 9, 2023, 11:15 AM

--- Core Contracts/modules/SaleModuleV1.sol
+++ Core Contracts/modules/SaleModuleV1.sol
@@ -35,7 +35,7 @@ contract CLDAuctionFactory {
         auctionList.push( Auction({ auctionAddress: address(newInstance), startDate: _startDate, endDate: _endDate, amountAuctioned: _AmountToAuction }) );//FIXME:
 
         emit NewAuction( address(newInstance), _startDate, _endDate, _AmountToAuction, _DevTeam );
-        return()
+        return(addressewInstance)
     }
 
     function setDAOAddress(address NewDAOAddress) external OnlyDAO {
