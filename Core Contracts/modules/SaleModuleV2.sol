8a8d1c3eded93e09bcfb8c87696474d1de38f1e2
Darius Tanz <scratchticketseb@gmail.com>
2023-01-10 15:11:36 +0000

    Jan 10, 2023, 10:11 AM

--- Core Contracts/modules/SaleModuleV2.sol
+++ Core Contracts/modules/SaleModuleV2.sol
@@ -1,8 +1,7 @@
 //SPDX-License-Identifier:UNLICENSE
 /* This contract is able to be replaced by the Winslow Core, and can also continue to be used 
 if a new Winslow Core is deployed by changing DAO addresses
-When setting up a new core or voting contract, ensure cross-compatibility and record keeping 
-done by the archive contract, voting index and proposal indexes never restart  */
+This contract provides a factory and sale contract for the WinslowCore*/
 pragma solidity ^0.8.17;
 
 contract SaleFactoryV2{
