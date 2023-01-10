09b2359547347c41699e03a2a44b0305b9f72c94
Darius Tanz <scratchticketseb@gmail.com>
2023-01-10 15:06:01 +0000

    Jan 10, 2023, 10:06 AM

--- Core Contracts/modules/SaleModuleV2.sol
+++ Core Contracts/modules/SaleModuleV2.sol
@@ -4,3 +4,4 @@ if a new Winslow Core is deployed by changing DAO addresses
 When setting up a new core or voting contract, ensure cross-compatibility and record keeping 
 done by the archive contract, voting index and proposal indexes never restart  TODO:Change This*/
 pragma solidity ^0.8.17;
+
