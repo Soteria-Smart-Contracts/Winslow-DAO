ae9d474d72da9af2f01eab9cfa8f91ac101a3ec3
Darius Tanz <scratchticketseb@gmail.com>
2023-01-10 15:03:46 +0000

    Jan 10, 2023, 10:03 AM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -1,6 +1,6 @@
 //SPDX-License-Identifier:UNLICENSE
 /* This contract is able to be replaced by itself, and can also continue to be used 
-if a new  Core is deployed by changing DAO addresses
+if a new external core is deployed by changing DAO addresses
 When setting up a new core or voting contract, ensure cross-compatibility and record keeping 
 done by the archive contract, voting index and proposal indexes never restart  TODO:Change This*/
 pragma solidity ^0.8.17;
