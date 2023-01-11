69bd8185e2ba0dc14492bf4ab4b0a06fec6f4d2d
Darius Tanz <scratchticketseb@gmail.com>
2023-01-11 18:17:51 +0000

    Jan 11, 2023, 1:17 PM

--- Core Contracts/modules/SaleModuleV2.sol
+++ Core Contracts/modules/SaleModuleV2.sol
@@ -35,7 +35,7 @@ contract SaleV2{
     //List of participants for front-end ranking
     address[] public ParticipantList; 
 
-    constructor(address Core, address CLD)
+    constructor(address _DAO, address _CLD)
 
 
 
