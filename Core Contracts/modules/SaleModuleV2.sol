7f4b0ac601622dd3694b8050769715f0289fa650
Darius Tanz <scratchticketseb@gmail.com>
2023-01-14 17:23:55 +0000

    Jan 14, 2023, 12:23 PM

--- Core Contracts/modules/SaleModuleV2.sol
+++ Core Contracts/modules/SaleModuleV2.sol
@@ -79,7 +79,7 @@ contract SaleV2{
         require(amount <= ParticipantDetails[msg.sender].EtherDeposited);
         require(SaleStatus() == SaleStatuses(1));
 
-        uint256 Fee = amount 
+        uint256 Fee = amount * 1
 
         return(success);
     }
