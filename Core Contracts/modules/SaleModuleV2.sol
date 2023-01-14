496466afa6350800c1958ce9f3196589a17a738a
Darius Tanz <scratchticketseb@gmail.com>
2023-01-14 16:35:14 +0000

    Jan 14, 2023, 11:35 AM

--- Core Contracts/modules/SaleModuleV2.sol
+++ Core Contracts/modules/SaleModuleV2.sol
@@ -73,7 +73,9 @@ contract SaleV2{
     //View Functions
 
     function SaleStatus() public view returns(SaleStatuses SaleStatus){
-        if(block.timestamp < starttime );
+        if(block.timestamp < starttime){
+
+        }
     }
 
 
