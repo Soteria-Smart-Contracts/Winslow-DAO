4086077ce2cf4a90da498e8724c76d4687523f48
Darius Tanz <scratchticketseb@gmail.com>
2023-01-14 17:21:31 +0000

    Jan 14, 2023, 12:21 PM

--- Core Contracts/modules/SaleModuleV2.sol
+++ Core Contracts/modules/SaleModuleV2.sol
@@ -75,7 +75,7 @@ contract SaleV2{
     }
 
     function WithdrawEther(uint256 amount) public returns(bool success){
-        require();
+        require(ParticipantDetails);
         require(SaleStatus() == SaleStatuses(1));
 
         return(success)
