d8ad2e14182b57b4f0f967163698e3bb60a4eda3
Darius Tanz <scratchticketseb@gmail.com>
2023-01-14 17:03:31 +0000

    Jan 14, 2023, 12:03 PM

--- Core Contracts/modules/SaleModuleV2.sol
+++ Core Contracts/modules/SaleModuleV2.sol
@@ -63,7 +63,7 @@ contract SaleV2{
         require(SaleStatus() == SaleStatuses(1));
         require(msg.value >= MinimumDeposit); 
 
-        ParticipantDetails[msg.sender]
+        ParticipantDetails[msg.sender].Paricipa
 
         return(success);
     }
