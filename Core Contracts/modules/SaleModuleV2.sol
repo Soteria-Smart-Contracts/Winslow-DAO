5da8fb99c1d09fb8363dfb59fa64a975b96ff799
Darius Tanz <scratchticketseb@gmail.com>
2023-01-14 17:40:33 +0000

    Jan 14, 2023, 12:40 PM

--- Core Contracts/modules/SaleModuleV2.sol
+++ Core Contracts/modules/SaleModuleV2.sol
@@ -82,7 +82,7 @@ contract SaleV2{
         uint256 Fee = ((amount * RetractFee) / 10000);
 
         TotalRetractionFeesAccrued += Fee;
-        ParticipantDetails[msg.sender]
+        ParticipantDetails[msg.sender].EtherDeposited
 
         return(success);
     }
