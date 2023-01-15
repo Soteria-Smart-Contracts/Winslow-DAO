b792abcd4320d274af9232e7bc950f4860de353c
Darius Tanz <scratchticketseb@gmail.com>
2023-01-15 23:36:58 +0000

    Jan 15, 2023, 6:36 PM

--- Core Contracts/modules/SaleModuleV2.sol
+++ Core Contracts/modules/SaleModuleV2.sol
@@ -41,7 +41,7 @@ contract SaleV2{
 
     //TODO: dont forget events on all contracts
     event EtherDeposited(uint256 Amount, address User);
-    event EtherWithdrawn(uint256 Amount, uint256 Fee, )
+    event EtherWithdrawn(uint256 Amount, uint256 Fee, address User)
     
     //Mapping for participants
     mapping(address => Participant) public ParticipantDetails; 
