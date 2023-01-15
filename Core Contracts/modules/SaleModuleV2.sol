1e6d95a9b885a1be0011d561bca69fc16ff5cfa3
Darius Tanz <scratchticketseb@gmail.com>
2023-01-15 22:26:24 +0000

    Jan 15, 2023, 5:26 PM

--- Core Contracts/modules/SaleModuleV2.sol
+++ Core Contracts/modules/SaleModuleV2.sol
@@ -96,7 +96,7 @@ contract SaleV2{
         ParticipantDetails[msg.sender].CLDclaimed = true;
 
 
-        uint256 CLDtoSend = ((CLDToBeSold *  ParticipantDetails[msg.sender].EtherDeposited) / TotalEtherPool)
+        uint256 CLDtoSend = ((CLDToBeSold *  ParticipantDetails[msg.sender].EtherDeposited) / TotalEtherPool);
         ParticipantDetails[msg.sender].CLDWithdrawn = CLDtoSend;
 
 
