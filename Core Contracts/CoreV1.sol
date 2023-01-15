5fd15d644d09a9d28a87291ee91b9704847887db
Darius Tanz <scratchticketseb@gmail.com>
2023-01-15 22:32:22 +0000

    Jan 15, 2023, 5:32 PM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -270,7 +270,7 @@ contract Winslow_Core_V1{
 
 
     // Other Internals
-
+    //TODO: Make the cuts changeable
     function ReceiveProposalCost() internal returns(bool success){
 
         ERC20(CLDAddress()).transferFrom(msg.sender, TreasuryContract, (ProposalCost / 2));
