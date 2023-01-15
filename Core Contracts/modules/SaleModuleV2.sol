9b9c8623a2352a23dc45f367b11e955e2b30e1c8
Darius Tanz <scratchticketseb@gmail.com>
2023-01-15 22:09:14 +0000

    Jan 15, 2023, 5:09 PM

--- Core Contracts/modules/SaleModuleV2.sol
+++ Core Contracts/modules/SaleModuleV2.sol
@@ -92,8 +92,8 @@ contract SaleV2{
     function ClaimCLD() public returns(bool success, uint256 AmountClaimed){
         require(ParticipantDetails[msg.sender].Participated == true);
         require(ParticipantDetails[msg.sender].CLDclaimed == false);
+        require(SaleStatus() == SaleStatuses(2));
 
-        
 
 
     }
