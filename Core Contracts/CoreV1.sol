09cf7840df30cd2ab6c05154eebf4d6257b2a193
Darius Tanz <scratchticketseb@gmail.com>
2023-01-16 00:08:14 +0000

    Jan 15, 2023, 7:08 PM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -288,13 +288,13 @@ contract Winslow_Core_V1{
         require(msg.sender == InitialSetter);
         require(InitialContractsSet == false);
 
-        TreasuryContract = TreasuryAddress;
+        TreasuryContract = _TreasuryAddress;
         VotingContract = VotingAddress;
         InitialSetter = address(0); //Once the reasury address has been set for the first time, it can only be set again via proposal 
         InitialContractsSet = true;
         ActiveContract = true;
 
-        emit NewTreasurySet(TreasuryAddress);
+        emit NewTreasurySet(_TreasuryAddress);
         
     }
 
