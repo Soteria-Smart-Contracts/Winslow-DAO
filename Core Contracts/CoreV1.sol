e6bf4825cf7427544f91f39417bffa04e4019246
Darius Tanz <scratchticketseb@gmail.com>
2023-01-18 14:23:14 +0000

    Jan 18, 2023, 9:23 AM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -328,8 +328,7 @@ interface Voting{
 
 interface Replacements{
     function InheritCore(address Treasury, address Voting, uint256 LatestProposal, uint256 ProposalCost) external returns(bool success);
-    function SendPreviousVoting(address OldVoting) external returns(bool success);
-    function SendPreviousTreasury(address OldTreasury) external returns(bool success);
+    function SendPreviousVoting(address Precessesor) external returns(bool success);
     function ChangeDAO(address NewDAO) external returns(bool success);
 
 }
