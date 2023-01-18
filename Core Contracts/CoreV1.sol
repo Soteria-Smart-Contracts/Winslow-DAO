a4c6d2a1e559a0ce4ca4e409b0fcaf6cb450f339
Darius Tanz <scratchticketseb@gmail.com>
2023-01-18 14:08:40 +0000

    Jan 18, 2023, 9:08 AM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -27,7 +27,7 @@ contract Winslow_Core_V1{
     uint256 MRIdentifier = 0;
 
     //Token Sales
-    mapping(uint256 => )
+    mapping(uint256 => address) public 
 
     enum ProposalStatus{
         Security_Verification,
