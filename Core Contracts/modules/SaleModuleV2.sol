64d9bd30b213b2858a74fda94cc5677e651403ad
Darius Tanz <scratchticketseb@gmail.com>
2023-01-18 14:14:56 +0000

    Jan 18, 2023, 9:14 AM

--- Core Contracts/modules/SaleModuleV2.sol
+++ Core Contracts/modules/SaleModuleV2.sol
@@ -72,7 +72,7 @@ contract SaleV2{
     constructor(address _DAO, uint256 SaleID, uint256 CLDtoSell, uint256 SaleLength, uint256 FoundationFee, uint256 RetractionFee, uint256 MinDeposit){
         require(SaleLength >= 259200 && SaleLength <= 1209600);
         DAO = _DAO;
-        SaleId
+        SaleIdentifier = SaleID;
         CLD = Core(DAO).CLDAddress();
         CLDToBeSold = CLDtoSell; //Make sure CLD is transfered to contract by treasury, additional CLD sent to the sale contract will be lost
         StartTime = block.timestamp + 43200;
