9fa24a08d9663e19a2500653d780dd5bd89ffe6a
Darius Tanz <scratchticketseb@gmail.com>
2023-02-13 14:09:00 +0000

    Feb 13, 2023, 9:09 AM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -308,6 +308,7 @@ contract Winslow_Core_V1{
 
         TreasuryContract = _TreasuryAddress;
         VotingContract = _VotingAddress;
+        SaleFactory = _SaleFactory;
         FoundationAddress = _FoundationAddress;
         InitialSetter = address(0); //Once the reasury address has been set for the first time, it can only be set again via proposal 
         InitialContractsSet = true;
