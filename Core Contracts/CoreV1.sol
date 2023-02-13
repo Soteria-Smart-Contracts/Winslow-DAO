510930394a6c3c0bb8fae40ea14c4848bd41fd44
Darius Tanz <scratchticketseb@gmail.com>
2023-02-13 17:47:39 +0000

    Feb 13, 2023, 12:47 PM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -30,7 +30,9 @@ contract Winslow_Core_V1{
     //Token Sales
     mapping(uint256 => Sale) public Sales;
     bool public ActiveSale; //TODO: Replace with function that checks to see if the most recent sale is still active
-    function SaleActive() public view returns(uint256 )
+    function SaleActive() public view returns(bool){
+        
+    }
     uint256 public LatestSale;
 
     enum ProposalStatus{
