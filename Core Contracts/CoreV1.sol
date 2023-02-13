b428018572e4be47dec4336b6ae92219a48c8e42
Darius Tanz <scratchticketseb@gmail.com>
2023-02-13 17:42:32 +0000

    Feb 13, 2023, 12:42 PM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -355,7 +355,9 @@ interface SaleFactory{
 }
 
 interface Sale{
-    
+    function name(type name) {
+        
+    }
 }
 
 //Only for the first treasury, if the DAO contract is not updated but the treasury is in the future, only Eros proposals will be able to access it due to their flexibility
