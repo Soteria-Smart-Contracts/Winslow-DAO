9ba7d98637cb6937351dc67de540beb531222eea
Darius Tanz <scratchticketseb@gmail.com>
2023-02-13 14:13:09 +0000

    Feb 13, 2023, 9:13 AM

--- Core Contracts/modules/SaleModuleV2.sol
+++ Core Contracts/modules/SaleModuleV2.sol
@@ -21,7 +21,7 @@ contract SaleFactoryV2{
     }
 
     function CreateNewSale(uint256 SaleID, uint256 CLDtoSell) external OnlyDAO returns(address _NewSaleAddress){
-        require(ERC20(Core(DAO).CLDAddress()).balanceOf(Core(DAO).TreasuryContract()) >= CLDtoSell);
+        require(ERC20(Core(DAO).CLDAddress()).balanceOf(Core(DAO).TreasuryContract()) >= CLDtoSell && );
         address NewSaleAddress = address(new SaleV2(DAO, SaleID, CLDtoSell, SaleLength, FoundationFee, RetractFee, MinimumDeposit));
         
         return(NewSaleAddress);
