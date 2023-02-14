08aa1ad8653aaf8fa1490f183f61828d83849ae9
Darius Tanz <scratchticketseb@gmail.com>
2023-02-14 22:14:57 +0000

    Feb 14, 2023, 5:14 PM

--- Core Contracts/modules/SaleModuleV2.sol
+++ Core Contracts/modules/SaleModuleV2.sol
@@ -39,7 +39,7 @@ contract SaleFactoryV2{
         require(DAO != newAddr, "VotingSystemV1.ChangeDAO: New DAO address can't be the same as the old one");
         require(address(newAddr) != address(0), "VotingSystemV1.ChangeDAO: New DAO can't be the zero address");
         DAO = newAddr;    
-        emit NewDAOAddress(newAddr);
+        //emit NewDAOAddress(newAddr); TODO: Events
     }
 
 }
