fbffbb8d50b17ec8536c38c1813235a9523346f3
Darius Tanz <scratchticketseb@gmail.com>
2023-02-14 22:29:40 +0000

    Feb 14, 2023, 5:29 PM

--- Core Contracts/modules/SaleModuleV2.sol
+++ Core Contracts/modules/SaleModuleV2.sol
@@ -179,7 +179,7 @@ contract SaleV2{
     //DAO Only functions
 
     function VerifyReadyForSale() external OnlyDAO returns(bool Ready){
-        require(ERC20(CLD).balanceOf(address(this) == CLDToBeSold);
+        require(ERC20(CLD).balanceOf(address(this)CLDToBeSold);
     }
 
     //View Functions
