dea3810fecf431ce2f9af0a44f6b3509dbd18d67
Darius Tanz <scratchticketseb@gmail.com>
2023-03-21 13:02:56 +0000

    Mar 21, 2023, 9:02 AM

--- Core Contracts/modules/SaleModuleV2.sol
+++ Core Contracts/modules/SaleModuleV2.sol
@@ -18,7 +18,7 @@ contract SaleFactoryV2{
     }
 
     //Events
-    event NewSaleCreated(uint256 SaleID, )
+    event NewSaleCreated(uint256 SaleID, uint256 SaleAmount, address )
 
     modifier OnlyDAO{ 
         require(msg.sender == address(DAO), 'This can only be done by the DAO');
