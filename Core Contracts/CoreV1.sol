178fc30d5d0eb8d9dd6a058dfea9b5736191c834
Darius Tanz <scratchticketseb@gmail.com>
2023-03-22 13:22:09 +0000

    Mar 22, 2023, 9:22 AM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -221,6 +221,7 @@ contract Winslow_Core_V1{
         MRIdentifier++;
 
         if(EROS(Slot).Multi() ==  true){
+            require(condition);
             uint256 VotingInstanceID = Voting(VotingContract).InitializeVoteInstance(NewIdentifier, VotingLength, true);
             Proposals[NewIdentifier] = Proposal(NewIdentifier, Slot, Memo, ProposalStatus(0), SecurityStatus(0), ProposalTypes(2), SimpleProposalTypes(0), VotingInstanceID, VotingLength, RequestedEther, RequestedAssetAmount, RequestedAssetID, EmptyProxy, true, EROS(Slot).OptionCount(), false, msg.sender);
         }
