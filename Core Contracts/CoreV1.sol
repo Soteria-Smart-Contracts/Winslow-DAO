86d7a8bf6797741113bd4a7a41d9b7fc529af35b
Darius Tanz <scratchticketseb@gmail.com>
2023-03-22 13:24:50 +0000

    Mar 22, 2023, 9:24 AM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -221,7 +221,7 @@ contract Winslow_Core_V1{
         MRIdentifier++;
 
         if(EROS(Slot).Multi() ==  true){
-            require(EROS(Slot).OptionCount() > 1, 'Eros proposal marked as multiple options true, but less than ');
+            require(EROS(Slot).OptionCount() > 1, 'Eros proposal marked as multiple options true, but less than two options are available');
             uint256 VotingInstanceID = Voting(VotingContract).InitializeVoteInstance(NewIdentifier, VotingLength, true);
             Proposals[NewIdentifier] = Proposal(NewIdentifier, Slot, Memo, ProposalStatus(0), SecurityStatus(0), ProposalTypes(2), SimpleProposalTypes(0), VotingInstanceID, VotingLength, RequestedEther, RequestedAssetAmount, RequestedAssetID, EmptyProxy, true, EROS(Slot).OptionCount(), false, msg.sender);
         }
