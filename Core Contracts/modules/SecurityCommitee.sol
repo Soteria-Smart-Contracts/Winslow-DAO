09bf996307847a77dc5b715d56f78d83df55cbf0
Darius Tanz <scratchticketseb@gmail.com>
2023-03-22 18:15:39 +0000

    Mar 22, 2023, 2:15 PM

--- Core Contracts/modules/SecurityCommitee.sol
+++ Core Contracts/modules/SecurityCommitee.sol
@@ -1,2 +1,5 @@
 //SPDX-License-Identifier:UNLICENSE
+/* This contract is able to be replaced by the Winslow Core, and can also continue to be used 
+if a new Winslow Core is deployed by changing DAO addresses
+This contract*/
 pragma solidity ^0.8.17;
