a97137719226d914b4d8581c0988e338e0d53e8b
Darius Tanz <scratchticketseb@gmail.com>
2023-03-23 14:20:39 +0000

    Mar 23, 2023, 10:20 AM

--- Core Contracts/modules/SecurityCommitee.sol
+++ Core Contracts/modules/SecurityCommitee.sol
@@ -30,5 +30,5 @@ contract Winslow_Security_Commitee_V1{
 
     //View Functions
 
-    function AllMembers() public view returns()
+    function AllMembers() public view returns(address[] Members)
 }
