0dc3cf940471bd45815d9a1ddb7d13746771372e
Darius Tanz <scratchticketseb@gmail.com>
2023-03-23 12:30:36 +0000

    Mar 23, 2023, 8:30 AM

--- Core Contracts/modules/SecurityCommitee.sol
+++ Core Contracts/modules/SecurityCommitee.sol
@@ -5,9 +5,8 @@ This contract allows and keeps track of commitee member votes on safety ratings 
 pragma solidity ^0.8.17;
 
 contract Winslow_Security_Commitee_V1{
-    //Declari
+    //Variable and other Declarations
     address DAO;
-
     uint256 TotalMembers;
     address[] CommiteeMembers;
     
