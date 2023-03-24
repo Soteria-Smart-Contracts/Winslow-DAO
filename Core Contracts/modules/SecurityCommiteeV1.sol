1843c967dbc6837ee86cdd7b247a2c01b000af07
Darius Tanz <scratchticketseb@gmail.com>
2023-03-24 17:35:35 +0000

    Mar 24, 2023, 1:35 PM

--- Core Contracts/modules/SecurityCommiteeV1.sol
+++ Core Contracts/modules/SecurityCommiteeV1.sol
@@ -35,7 +35,7 @@ contract Winslow_Security_Commitee_V1{
 
     //DAO Only Functions
     
-    function AddMember(address Member) external OnlyDAO returns()
+    function AddMember(address Member) external OnlyDAO returns(bool success)
 
     function RemoveMember(address Member)
 
