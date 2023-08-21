2c35073a07ca532151e2e11d5bde29528cd4031a
Darius Tanz <scratchticketseb@gmail.com>
2023-08-21 14:01:02 +0000

    Aug 21, 2023, 10:01 AM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -338,7 +338,7 @@ contract Winslow_Core_V1 {
 
     function ChangeSaleFoundationFee(uint256 NewFee) internal returns(bool success){
             
-        SaleFactory(SaleFactoryContract).ChangeFoundationFee();
+        SaleFactory(SaleFactoryContract).ChangeFoundationFee(NewFee);
         return(success);
 
     }
