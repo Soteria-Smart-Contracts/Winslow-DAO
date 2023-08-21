046ef998ce33cbec63aedf2146a8637b3740a4d3
Darius Tanz <scratchticketseb@gmail.com>
2023-08-21 14:00:47 +0000

    Aug 21, 2023, 10:00 AM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -406,18 +406,17 @@ interface Replacements{
 interface SaleFactory{
     function CreateNewSale(uint256 SaleID, uint256 CLDtoSell) external returns(address NewSaleContract);
     function MaximumSalePercentage() external returns(uint256 BasisPointMax);
-}
-
-interface SaleContract{
-    function StartTime() external returns(uint256 Time);
-    function EndTime() external returns(uint256 Time);
-    function VerifyReadyForSale() external returns(bool Ready);
     function ChangeFoundationFee(uint256 NewFee) external returns(bool success);
     function ChangeRetractFee(uint256 NewRetractFee) external returns(bool success);
     function ChangeMinimumDeposit(uint256 NewMinDeposit) external returns(bool success);
     function ChangeDefaultSaleLength(uint256 NewLength) external returns(bool success);
     function ChangeMaxSalePercent(uint256 NewMaxPercent) external returns(bool success);
-    
+}
+
+interface SaleContract{
+    function StartTime() external returns(uint256 Time);
+    function EndTime() external returns(uint256 Time);
+    function VerifyReadyForSale() external returns(bool Ready); 
 }
 
 //Only for the first treasury, if the DAO contract is not updated but the treasury is in the future, only Eros proposals will be able to access it due to their flexibility
