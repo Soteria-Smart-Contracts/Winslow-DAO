11b3b6a6a2bd97f462e74127837c3b3016becabe
Darius Tanz <scratchticketseb@gmail.com>
2023-11-12 14:31:29 +0000

    Nov 12, 2023, 9:31 AM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -549,7 +549,18 @@ interface SaleFactory{
 }
 
 interface ProxyContract{
-    
+    struct ProxyProposalArguments{
+        uint8 FunctionSelector;
+        uint256 UnsignedInt1;
+        uint256 UnsignedInt2;
+        uint256 UnsignedInt3; 
+        address Address1;
+        address Address2;
+        address Address3;
+        bool Bool1;
+        bool Bool2;
+        bool Bool3;
+    }
 
     function ProxyFunctionOne(ProxyProposalArguments memory ExecutionArguments) external returns(bool success);
     function ProxyFunctionTwo(ProxyProposalArguments memory ExecutionArguments) external returns(bool success);
