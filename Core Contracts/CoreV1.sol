2754e3808ecb03159d9afc66d8a94882300f9d6e
Darius Tanz <scratchticketseb@gmail.com>
2023-11-12 14:24:56 +0000

    Nov 12, 2023, 9:24 AM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -335,19 +335,7 @@ contract Winslow_Core_V1 {
 
         if(ProxyArgs[ProposalID].FunctionSelector == 1){
             ProxyProposal(Proposals[ProposalID].AddressSlot).ProxyFunctionOne();
-        }
-        else if(ProxyArgs[ProposalID].FunctionSelector == 2){
-            ProxyProposal(Proposals[ProposalID].AddressSlot).ProxyFunctionTwo();
-        }
-        else if(ProxyArgs[ProposalID].FunctionSelector == 3){
-            ProxyProposal(Proposals[ProposalID].AddressSlot).ProxyFunctionThree();
-        }
-        else if(ProxyArgs[ProposalID].FunctionSelector == 4){
-            ProxyProposal(Proposals[ProposalID].AddressSlot).ProxyFunctionFour();
-        }
-        else if(ProxyArgs[ProposalID].FunctionSelector == 5){
-            ProxyProposal(Proposals[ProposalID].AddressSlot).ProxyFunctionFive();
-        }
+    
        
 
 
