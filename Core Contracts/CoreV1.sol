65b2e6961d909f1cc3056bd8b3ef80a04889a145
Darius Tanz <scratchticketseb@gmail.com>
2023-12-11 18:35:57 +0000

    Dec 11, 2023, 1:35 PM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -134,7 +134,8 @@ contract Winslow_Core_V1 {
     event NewTreasurySet(address NewTreasury);
     //create all the events we need and in the following line for each add a comment and the line or lines it should be inserted into
 
-    
+    event NewVotingSet(address NewVoting);
+    //line  114
 
 
     constructor(){
