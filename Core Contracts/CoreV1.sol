4c7f1655d2f4aed840163853c1fbb4cae3cec6bb
Darius Tanz <scratchticketseb@gmail.com>
2023-12-12 21:52:20 +0000

    Dec 12, 2023, 4:52 PM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -392,8 +392,7 @@ contract Winslow_Core_V1 {
         // Execute the eros proposal
         EROS(Proposals[ProposalID].AddressSlot).Execute();
 
-        //what else needs to be done such as updating the proposal info and proposal struct?
-        //
+        
     }
 
     //  Internal Simple Proposal Call Functions
