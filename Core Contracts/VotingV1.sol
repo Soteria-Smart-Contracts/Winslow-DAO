d06288889cff7de919e2413d73e659ef86d36d17
Darius Tanz <scratchticketseb@gmail.com>
2023-12-13 16:51:04 +0000

    Dec 13, 2023, 11:51 AM

--- Core Contracts/VotingV1.sol
+++ Core Contracts/VotingV1.sol
@@ -129,6 +129,7 @@ contract Winslow_Voting_V1 {
         VoterInfo[VotingInstance][msg.sender].VotesLocked += amount;
         VoterInfo[VotingInstance][msg.sender].Voted = true;
         VotingInstances[VotingInstance].Voters.push(msg.sender);
+        
 
         return(success);
     }
