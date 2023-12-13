2c170a865a4947a1fce1631214bbee7528fbfb34
Darius Tanz <scratchticketseb@gmail.com>
2023-12-13 17:38:39 +0000

    Dec 13, 2023, 12:38 PM

--- Core Contracts/VotingV1.sol
+++ Core Contracts/VotingV1.sol
@@ -201,9 +201,9 @@ contract Winslow_Voting_V1 {
         require(VoterInfo[VotingInstance][msg.sender].CLDReturned == false);
         require(block.timestamp >= VotingInstances[VotingInstance].VoteEnds, "VotingSystemV1.ReturnTokens: Voting has not ended for this instance");
 
+        index 
         UserUnreturnedVotes[msg.sender][index] = UserUnreturnedVotes[msg.sender][UserUnreturnedVotes[msg.sender].length - 1];
         UserUnreturnedVotesIndex[msg.sender][UserUnreturnedVotes[msg.sender][index]] = index;
-        
         VoterInfo[VotingInstance][msg.sender].CLDReturned = true;
 
         uint256 TotalToReturn;
