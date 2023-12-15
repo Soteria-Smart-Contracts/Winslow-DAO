f57157056da577bb141315f0a1e44019845d732d
Darius Tanz <scratchticketseb@gmail.com>
2023-12-15 16:03:43 +0000

    Dec 15, 2023, 11:03 AM

--- Core Contracts/VotingV1.sol
+++ Core Contracts/VotingV1.sol
@@ -296,6 +296,7 @@ contract Winslow_Voting_V1 {
         require(block.timestamp >= VotingInstances[VotingQueue[0]].VoteStarts, "VotingSystemV1.BeginNextVote: The first proposal in the queue is not ready to be voted on");
 
         //loop through the queue to find the proposal with the highest incentive, begin it and remove it from the queue
+
         
 
 
