c61faee27fe89e0dae6781226ce16e728d5ba497
Darius Tanz <scratchticketseb@gmail.com>
2023-12-15 15:21:09 +0000

    Dec 15, 2023, 10:21 AM

--- Core Contracts/VotingV1.sol
+++ Core Contracts/VotingV1.sol
@@ -289,7 +289,7 @@ contract Winslow_Voting_V1 {
         //check if the current vote is over, or if there is no current vote as it is the first
         if(CurrentOngoingVote != 0){
             require(block.timestamp >= VotingInstances[CurrentOngoingVote].VoteEnds, "VotingSystemV1.BeginNextVote: The current vote is not over");
-        }else if()
+        }
         require(block.timestamp >= VotingInstances[VotingQueue[0]].VoteStarts, "VotingSystemV1.BeginNextVote: The first proposal in the queue is not ready to be voted on");
 
         CurrentOngoingVote = VotingQueue[0];
