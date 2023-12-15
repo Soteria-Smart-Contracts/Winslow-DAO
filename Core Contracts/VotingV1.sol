d61ece9a682bd0b55ca668813879e68feaa8e1db
Darius Tanz <scratchticketseb@gmail.com>
2023-12-15 18:00:00 +0000

    Dec 15, 2023, 1:00 PM

--- Core Contracts/VotingV1.sol
+++ Core Contracts/VotingV1.sol
@@ -12,7 +12,7 @@ contract Winslow_Voting_V1 {
     // Contracts and Routing Variables
     string public Version = "V1";
     address public DAO;
-    uint256 public Quorum = ;
+    uint256 public Quorum = 1500000000000000000000;
 
     // Percentages in Basis Points
     uint256 public ExecutorCut;
