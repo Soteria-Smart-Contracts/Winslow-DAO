646c717d23b7f90f190f878abbfc517a569cbed1
Darius Tanz <scratchticketseb@gmail.com>
2023-12-15 17:57:51 +0000

    Dec 15, 2023, 12:57 PM

--- Core Contracts/VotingV1.sol
+++ Core Contracts/VotingV1.sol
@@ -243,9 +243,9 @@ contract Winslow_Voting_V1 {
 
     //TODO: GetVotingResult
 
-    function GetVotingResult(uint256 ) public view returns(bool Result, uint8 Multi){
+    function GetVotingResult(uint256 VoteInstance) public view returns(bool Result, uint8 Multi){
         require(CurrentOngoingVote != 0, "VotingSystemV1.GetVotingResult: There is no current vote");
-        require(block.timestamp >= VotingInstances[CurrentOngoingVote].VoteEnds, "VotingSystemV1.GetVotingResult: The current vote is not over");
+        require(block.timestamp >= VotingInstances[VoteInstance].VoteEnds, "VotingSystemV1.GetVotingResult: The current vote is not over");
 
 
 
