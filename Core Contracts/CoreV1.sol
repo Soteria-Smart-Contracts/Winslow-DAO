bac5ce06bdec80b9ca7503338b3c897a7a34068a
Darius Tanz <scratchticketseb@gmail.com>
2023-12-16 17:45:14 +0000

    Dec 16, 2023, 12:45 PM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -916,7 +916,7 @@ contract Winslow_Voting_V1 {
     }
 
     function ChangeQuorum(uint256 newQuorum) external OnlyDAO returns(bool success){
-        require(newQuorum > 250000000000000000000); //
+        require(newQuorum > 250000000000000000000); //Minimum quorum is 250 tokens but 
 
         Quorum = newQuorum;
         
