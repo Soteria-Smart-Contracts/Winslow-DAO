7eed9c009f61ecd11094afaa90a174df2e200b0a
Darius Tanz <scratchticketseb@gmail.com>
2023-12-16 16:12:39 +0000

    Dec 16, 2023, 11:12 AM

--- Core Contracts/VotingV1.sol
+++ Core Contracts/VotingV1.sol
@@ -244,6 +244,7 @@ contract Winslow_Voting_V1 {
 
     function GetVotingResult(uint256 _VoteInstance) public view returns(bool Result, uint8 Multi){
         require(block.timestamp >= VotingInstances[_VoteInstance].VoteEnds, "VotingSystemV1.GetVotingResult: The current vote is not over");
+
         //if the total votes does not meet the quorum, the vote fails and returns false
         if(VotingInstances[_VoteInstance].TotalCLDVoted < Quorum){
             return(false, Multi);
