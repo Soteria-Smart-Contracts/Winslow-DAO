ea7fe53a60e751392bab767f9ac09971f29d9e51
Darius Tanz <scratchticketseb@gmail.com>
2023-12-16 16:37:32 +0000

    Dec 16, 2023, 11:37 AM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -985,16 +985,6 @@ interface SaleContract{
     function VerifyReadyForSale() external returns(bool Ready); 
 }
 
-//Only for the first treasury, if the DAO contract is not updated but the treasury is in the future, only Eros proposals will be able to access it due to their flexibility
-import "./CoreV1.sol";
-
-//SPDX-License-Identifier:UNLICENSE
-/* This contract is able to be replaced by the Winslow Core, and can also continue to be used 
-if a new Winslow Core is deployed by changing DAO addresses
-When setting up a new core or voting contract, ensure cross-compatibility and record keeping 
-done by the archive contract, voting index and proposal indexes never restart  TODO:Change This*/
-pragma solidity ^0.8.17;
-
 contract Winslow_Treasury_V1 {
     //Variable, struct and type declarations
     string public Version = "V1";
