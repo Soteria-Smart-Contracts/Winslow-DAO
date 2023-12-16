731bc78b9dcf83e15e030243eebb641811ecc431
Darius Tanz <scratchticketseb@gmail.com>
2023-12-16 17:46:56 +0000

    Dec 16, 2023, 12:46 PM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -926,7 +926,7 @@ contract Winslow_Voting_V1 {
     function ChangeDAO(address newAddr) external OnlyDAO {
         require(DAO != newAddr, "VotingSystemV1.ChangeDAO: New DAO address can't be the same as the old one");
         require(address(newAddr) != address(0), "VotingSystemV1.ChangeDAO: New DAO can't be the zero address");
-        DAO = newAddr;    
+        DAO = payable(newAddr);    
         emit NewDAOAddress(newAddr);
     }
     
