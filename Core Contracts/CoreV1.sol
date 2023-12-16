41feaad71c04176f8ec246c2915dca906c2e8d23
Darius Tanz <scratchticketseb@gmail.com>
2023-12-16 17:50:32 +0000

    Dec 16, 2023, 12:50 PM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -661,7 +661,7 @@ contract Winslow_Voting_V1 {
     //Active Vote Functions
 
     function CastVote(uint256 amount, uint256 VotingInstance, Vote VoteChoice) external returns(bool success){
-        require(VotingInstances[VotingInstance].MultiOp == false);
+        require(VotingInstances[VotingInstance].MaxMulti == 0);
         require(ERC20(CLDAddress()).transferFrom(msg.sender, address(this), amount), "VotingSystemV1.CastVote: You do not have enough CLD to vote this amount or have not given the proper allowance to Winslow_Voting_V1");
         require(VoteChoice == Vote(0) || VoteChoice == Vote(1), "VotingSystemV1.CastVote: You must either vote YEA or NAY");
         require(amount >= 10000000000000000, "VotingSystemV1.CastVote: The minimum CLD per vote is 0.01"); //For incentive payout reasons
