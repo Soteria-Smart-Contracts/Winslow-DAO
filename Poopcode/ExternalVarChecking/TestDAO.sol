6376aa22ec04cb4cd6781129f1f05f67fc555b54
Darius Tanz <scratchticketseb@gmail.com>
2023-12-17 14:17:08 +0000

    Dec 17, 2023, 9:17 AM

--- Poopcode/ExternalVarChecking/TestDAO.sol
+++ Poopcode/ExternalVarChecking/TestDAO.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.17;
 contract WinslowDAOcompact{
 
     struct Proposal{
-        uint256 id;
-        uint256 voteCount;
+        uint256 ID;
+        uint256 YesVotes;
         string description;
         uint256 startTime;
         uint256 endTime;
