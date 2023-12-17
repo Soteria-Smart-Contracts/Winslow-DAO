3092105f010d9a13f84c641c5d24e62b4d9a0aea
Darius Tanz <scratchticketseb@gmail.com>
2023-12-17 14:33:05 +0000

    Dec 17, 2023, 9:33 AM

--- Poopcode/ExternalVarChecking/TestDAO.sol
+++ Poopcode/ExternalVarChecking/TestDAO.sol
@@ -45,7 +45,12 @@ contract WinslowDAOcompact{
 
 }
 
-interface ERC20{
-    function transfer(address recipient, uint256 amount) external returns (bool);
-    function balanceOf(address account) external view returns (uint256);
+interface ERC20 {
+  function balanceOf(address owner) external view returns (uint256);
+  function allowance(address owner, address spender) external view returns (uint256);
+  function approve(address spender, uint256 value) external returns (bool);
+  function transfer(address to, uint256 value) external returns (bool);
+  function transferFrom(address from, address to, uint256 value) external returns (bool); 
+  function totalSupply() external view returns (uint256);
+  function Burn(uint256 _BurnAmount) external;
 }
