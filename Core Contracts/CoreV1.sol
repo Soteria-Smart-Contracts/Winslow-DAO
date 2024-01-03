d636cb271c3ef578d8cea9bc2fc1edba407e9905
Darius Tanz <scratchticketseb@gmail.com>
2024-01-03 20:29:30 +0000

    Jan 3, 2024, 3:29 PM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -346,6 +346,7 @@ contract Winslow_Core_V1 {
         }
         else if(ProposalInfos[ProposalID].SimpleType == SimpleProposalTypes(11)){
             ChangeSaleRetractFee(Proposals[ProposalID].RequestedEtherAmount);
+            //guess what goes ne
         }
         else if(ProposalInfos[ProposalID].SimpleType == SimpleProposalTypes(12)){
             ChangeSaleMinimumDeposit(Proposals[ProposalID].RequestedEtherAmount); 
