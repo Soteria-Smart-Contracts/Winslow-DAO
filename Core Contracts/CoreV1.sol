db1e16889d72b7016687a8956c06b4992cdf7310
Darius Tanz <scratchticketseb@gmail.com>
2024-01-03 21:42:03 +0000

    Jan 3, 2024, 4:42 PM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -247,7 +247,7 @@ contract Winslow_Core_V1 {
         }
         else if(ProposalInfos[ProposalID].SimpleType == SimpleProposalTypes(2)){
             address TokenAddress = Proposals[ProposalID].AddressSlot;
-            uint8 Slot = uint8(ProxyArgs[ProposalID].UnsignedInt1);  //TODO: Fix when deleting proxy proposals
+            uint8 Slot = uint8();  //TODO: Fix when deleting proxy proposals
             Winslow_Treasury_V1(TreasuryContract).RegisterAsset(TokenAddress, Slot);
         }
         else if(ProposalInfos[ProposalID].SimpleType == SimpleProposalTypes(3)){
