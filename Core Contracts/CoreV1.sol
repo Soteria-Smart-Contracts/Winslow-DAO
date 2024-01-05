ed7b82ebcd3112ff9ac2b56c1aee889c54fb53f9
Darius Tanz <scratchticketseb@gmail.com>
2024-01-05 21:04:47 +0000

    Jan 5, 2024, 4:04 PM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -692,7 +692,14 @@ contract Winslow_Voting_V1 {
         }
 
         //execute or reject the proposal on the core contract
-        
+        if(VotingInstances[CurrentOngoingVote].Status == VoteStatus(2)){
+            if(VotingInstances[CurrentOngoingVote].YEAvotes > VotingInstances[CurrentOngoingVote].NAYvotes){
+                Winslow_Core_V1(DAO).ExecuteProposal(VotingInstances[CurrentOngoingVote].ProposalID);
+            }
+            else{
+                Winslow_Core_V1(DAO).RejectProposal(VotingInstances[CurrentOngoingVote].ProposalID);
+            }
+        }
 
         //loop through the queue to find the proposal with the highest incentive, begin it and remove it from the queue
         uint256 HighestIncentive = 0;
