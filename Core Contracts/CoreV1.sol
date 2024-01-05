7e4efbe1d465e106bf8eb611bd0c55e28198ebc6
Darius Tanz <scratchticketseb@gmail.com>
2024-01-05 21:16:03 +0000

    Jan 5, 2024, 4:16 PM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -233,38 +233,6 @@ contract Winslow_Core_V1 {
         return(success);
     }
 
-    function ExecuteProposal(uint256 ProposalID) external returns(bool success){
-        require(msg.sender == VotingContract, "Only the Winslow_Voting_V1 contract can execute proposals");
-        require(ProposalInfos[ProposalID].Status == ProposalStatus(1), "Proposal status must be voting to be executed");
-        (bool Result, uint8 Multi) = Winslow_Voting_V1(VotingContract).GetVoteResult(ProposalInfos[ProposalID].VotingInstanceID);
-        require(Result == true, "Proposal must be approved by Winslow_Voting_V1 to be executed");
-        require(Proposals[ProposalID].Executed == false, "Proposal has already been executed");
-    
-        Proposals[ProposalID].Executed = true;
-        ProposalInfos[ProposalID].Status = ProposalStatus(2);
-    
-        if(ProposalInfos[ProposalID].ProposalType == ProposalTypes(0)){
-            ExecuteSimpleProposal(ProposalID);
-        }
-        else if(ProposalInfos[ProposalID].ProposalType == ProposalTypes(1)){
-            ExecuteErosProposal(ProposalID, Multi);
-        }
-    
-        return(success);
-    }
-
-    function RejectProposal(uint256 ProposalID) external returns(bool success){
-        require(msg.sender == VotingContract, "Only the Winslow_Voting_V1 contract can reject proposals");
-        require(ProposalInfos[ProposalID].Status == ProposalStatus(1), "Proposal status must be voting to be rejected");
-        (bool Result, uint8 Multi) = Winslow_Voting_V1(VotingContract).GetVoteResult(ProposalInfos[ProposalID].VotingInstanceID);
-        require(Result == false, "Proposal must be rejected by Winslow_Voting_V1 to be rejected");
-        require(Proposals[ProposalID].Executed == false, "Proposal has already been executed");
-
-        ProposalInfos[ProposalID].Status = ProposalStatus(3);
-
-        return(success);
-    }
-
     //  Simple Executionting
 
     function ExecuteSimpleProposal(uint256 ProposalID) internal {
