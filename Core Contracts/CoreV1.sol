4f23669246f128f388100334bfbbcf50464c5277
Darius Tanz <scratchticketseb@gmail.com>
2024-01-05 23:29:32 +0000

    Jan 5, 2024, 6:29 PM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -445,8 +445,9 @@ contract Winslow_Voting_V1 {
         VotingIncomplete,
         VotingActive,
         VotingComplete
-    }
+    } 
 
+    event InstanceCreated()
     event VoteCast(address Voter, uint256 VotingInstance, string option, uint256 votesCasted);
     event ProposalIncentivized(address donator, uint256 VotingInstance, uint256 amountDonated);
     event TokensReturned(address Voter, uint256 TotalSent, uint256 IncentiveShare);
