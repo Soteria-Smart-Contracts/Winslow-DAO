683c5e1ea2bee3b3271fadbfac2e1acdad083508
Darius Tanz <scratchticketseb@gmail.com>
2024-01-05 20:43:59 +0000

    Jan 5, 2024, 3:43 PM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -217,14 +217,14 @@ contract Winslow_Core_V1 {
         Proposals[ProposalID].Executed = true;
         ProposalInfos[ProposalID].Status = ProposalStatus(2);
     
-            if(ProposalInfos[ProposalID].ProposalType == ProposalTypes(0)){
-                ExecuteSimpleProposal(ProposalID);
-            }
-            else if(ProposalInfos[ProposalID].ProposalType == ProposalTypes(1)){
-                ExecuteErosProposal(ProposalID, Multi);
-            }
+        if(ProposalInfos[ProposalID].ProposalType == ProposalTypes(0)){
+            ExecuteSimpleProposal(ProposalID);
+        }
+        else if(ProposalInfos[ProposalID].ProposalType == ProposalTypes(1)){
+            ExecuteErosProposal(ProposalID, Multi);
+        }
     
-            return(success);
+        return(success);
     }
 
     //reject proposal function
