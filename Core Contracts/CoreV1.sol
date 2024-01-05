e73c92cb1cb9d193df9756992d54f85cc7e9ea74
Darius Tanz <scratchticketseb@gmail.com>
2024-01-05 21:08:36 +0000

    Jan 5, 2024, 4:08 PM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -686,9 +686,8 @@ contract Winslow_Voting_V1 {
         //check if the current vote is over, or if there is no current vote as it is the first
         if(CurrentOngoingVote != 0){
             require(block.timestamp >= VotingInstances[CurrentOngoingVote].VoteEnds, "VotingSystemV1.BeginNextVote: The current vote is not over");
-            if(VotingInstances[CurrentOngoingVote].Status == VoteStatus(1)){
-                EndVoting(CurrentOngoingVote);
-            }
+            
+            EndVoting(CurrentOngoingVote);
         }
 
         //execute or reject the proposal on the core contract
