99467dc4cf9e59e4fffb8088ca6fce45c330d99c
Darius Tanz <scratchticketseb@gmail.com>
2024-02-14 21:40:28 +0000

    Feb 14, 2024, 4:40 PM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -111,7 +111,7 @@ contract Winslow_Core_V1 {
     constructor(){
         FoundationAddress = payable(0xc932b3a342658A2d3dF79E4661f29DfF6D7e93Ce); //TODO: Change this to the community agreed foundation address
 
-        DeployerContract = address(new WinslowDAODeployer());
+        DeployerContract = address(new WinslowAuxilliaryDeployer());
         SaleFactoryContract = address(new SaleFactoryV2());
         TreasuryContract = payable(address(new Winslow_Treasury_V1()));
         VotingContract = address(new Winslow_Voting_V1());
