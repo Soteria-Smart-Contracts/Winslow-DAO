fdfd8568352413523b16407550f26c9c3c26b43c
Darius Tanz <scratchticketseb@gmail.com>
2024-02-14 21:40:54 +0000

    Feb 14, 2024, 4:40 PM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -1177,7 +1177,7 @@ contract Winslow_Treasury_V1 {
     }
 }
 
-contract WinslowAuxiliaryDeployer{
+contract WinslowAuxilliaryDeployer{
     address public DAO;
     address public TreasuryDeployer;
     address public VotingDeployer;
