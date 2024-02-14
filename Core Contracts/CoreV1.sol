15b2000d8c0d8fd558c878ef0132df10b6b35664
Darius Tanz <scratchticketseb@gmail.com>
2024-02-14 21:46:05 +0000

    Feb 14, 2024, 4:46 PM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -1188,7 +1188,7 @@ contract WinslowAuxiliaryDeployer{
     }
 
     function DeployAuxiliaryContracts() external {
-        Winslow_Core_V1(DAO).SetAuxiliaryContracts(WinslowTreasuryDeployer(TreasuryDeployer).DeployTreasury(), );
+        Winslow_Core_V1(DAO).SetAuxiliaryContracts(WinslowTreasuryDeployer(TreasuryDeployer).DeployTreasury(), WinslowVotingDeployer(VotingDeployer).DeployVoting());
     }
 
 }
