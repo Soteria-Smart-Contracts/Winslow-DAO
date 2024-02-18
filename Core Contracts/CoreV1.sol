e7342f2232b0f65cf0a7c8fc2ffa3ef24ec5562a
Darius Tanz <scratchticketseb@gmail.com>
2024-02-18 00:47:08 +0000

    Feb 17, 2024, 7:47 PM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -217,11 +217,7 @@ contract Winslow_Core_V1 {
             ProposalInfos[ProposalID].Status = ProposalStatus(2);
             if(ProposalInfos[ProposalID].ProposalType == ProposalTypes(0)){
                 //try catch so that if the execution fails, the dao is not stuck
-                try this.ExecuteSimpleProposal(ProposalID) returns(bool success){
-                    return(success);
-                } catch {
-                    ProposalInfos[ProposalID].Status = ProposalStatus(3);
-                }
+                ExecuteSimpleProposal(ProposalID);
             }
             else if(ProposalInfos[ProposalID].ProposalType == ProposalTypes(1)){
                 ExecuteErosProposal(ProposalID, Multi);
