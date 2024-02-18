c16ea27ed1bf7f236ab1b5ab8625932f8c5a342a
Darius Tanz <scratchticketseb@gmail.com>
2024-02-18 19:30:20 +0000

    Feb 18, 2024, 2:30 PM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -726,7 +726,7 @@ contract Winslow_Voting_V1 {
         VotingQueue.pop();
         VotingQueueIndex[CurrentOngoingVote] = 0;
 
-        //Set
+        Winslow_Core_V1(DAO).SetProposalVoting(VotingInstances[CurrentOngoingVote].ProposalID);
         VotingInstances[CurrentOngoingVote].VoteStarts = (block.timestamp + 300); //TODO: Set to 43200 for 12 hours debate period
         VotingInstances[CurrentOngoingVote].VoteEnds = block.timestamp + Winslow_Core_V1(DAO).VoteLength();
         VotingInstances[CurrentOngoingVote].Status = VoteStatus(1);
