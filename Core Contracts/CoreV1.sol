1c2605351ab1e47d2055652bc3e451405aedbbbe
Darius Tanz <scratchticketseb@gmail.com>
2024-02-18 19:58:16 +0000

    Feb 18, 2024, 2:58 PM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -1077,7 +1077,7 @@ contract Winslow_Treasury_V1 {
             //It is very important that ERC20 contracts are audited properly to ensure that no errors could occur here, as one failed transfer would revert the whole TX
             if(RegisteredAssets[CurrentID].Filled == true){
                 uint256 ToSend = GetAssetToSend(CLDamount, CurrentID, SupplyPreTransfer);
-                ERC20(RegisteredAssets[CurrentID].TokenAddress).transfer(To, ToSend);
+                ERC20(RegisteredAssets[CurrentID].TokenAddress).transfer(To, ToSend); //TODO: Try Catch this
                 emit ERC20Sent(ToSend, To, tx.origin);
             }
             CurrentID++;
