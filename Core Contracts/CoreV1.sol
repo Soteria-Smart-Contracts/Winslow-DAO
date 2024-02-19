0ac9fdd3b3ba19c166d0c9b6c0ec514daf6c91fe
Darius Tanz <scratchticketseb@gmail.com>
2024-02-19 21:26:14 +0000

    Feb 19, 2024, 4:26 PM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -615,7 +615,11 @@ contract Winslow_Voting_V1 {
         return(VotingInstances[_VoteInstance]);
     }
 
-    //
+    //get the proposal queue
+
+    function GetVotingQueue() public view returns(uint256[] memory){
+        return(VotingQueue);
+    }
 
     function GetVoteResult(uint256 _VoteInstance) public view returns(bool Result, uint8 Multi){
         require(block.timestamp >= VotingInstances[_VoteInstance].VoteEnds, "VotingSystemV1.GetVotingResult: The current vote is not over");
