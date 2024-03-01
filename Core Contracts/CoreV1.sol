22febff55163882593a17dc523c779f89b27ad5e
Darius Tanz <scratchticketseb@gmail.com>
2024-03-01 16:47:16 +0000

    Mar 1, 2024, 11:47 AM

--- Core Contracts/CoreV1.sol
+++ Core Contracts/CoreV1.sol
@@ -144,7 +144,6 @@ contract Winslow_Core_V1 {
     //  Internal Functions
 
     function InitializeSimpleProposal(string memory Memo, address AddressSlot, SimpleProposalTypes SimpleType, uint256 RequestedEther, uint256 RequestedAssetAmount, uint8 RequestedAssetID) internal returns(uint256 Identifier){
-        require(SimpleType != SimpleProposalTypes(0), "Simple proposals cannot be of type 0");
         //require that there are no more than 100 proposals in the voting queue on the voting contract, if there are, the DAO must execute some before more can be added, this is to prevent gas issues for beginnextvote
         require(Winslow_Voting_V1(VotingContract).GetVotingQueueSize() < 100, "There are too many proposals in the queue, the DAO must execute some before more can be added");
 
